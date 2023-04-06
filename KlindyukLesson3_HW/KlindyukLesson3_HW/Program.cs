12cb655091cbba85da5601c959af60b8c56e4228
ManBanshee <klindyuk98@gmail.com>
2023-04-07 00:57:01 +0300

    Initial Commit

---
+++ KlindyukLesson3_HW/KlindyukLesson3_HW/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlindyukLesson3_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
