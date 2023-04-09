ea2b7392a0ff49f9b97cb06440068d6ac02f6f95
Молодой Дед <129785893+ZedDed0k@users.noreply.github.com>
2023-04-09 12:57:34 +0300

    Add files via upload

---
+++ HOMEWORK/Calc/Calc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programm // Без класса у меня 4 раза вылетела консоль и Вижла вместе с ней
+{
+    static void Main()
+    {
+        var a = 5;
+        var b = 10;
+
+        // Сложение
+        var sum = a + b;
+        Console.WriteLine($"Сумма: {sum}");
+
+        // Вычитание
+        var difference = a - b;
+        Console.WriteLine($"Разность: {difference}");
+
+        // Умножение
+        var product = a * b;
+        Console.WriteLine($"Произведение: {product}");
+
+        // Деление
+        var quotient = a / b;
+        Console.WriteLine($"Частное: {quotient}");
+
+        // Возведение в степень
+        var power = Math.Pow(a, b);
+        Console.WriteLine($"Степень: {power}");
+
+        Console.ReadKey();
+    }
+
+}
