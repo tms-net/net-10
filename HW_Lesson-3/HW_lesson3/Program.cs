fc338a2aee8ef8689fb316cbb10a814919d32a2d
Evgeny <69685960+EvgenyLipayMinsk@users.noreply.github.com>
2023-04-10 10:21:16 +0300

    Fixed bugs

--- HW_Lesson-3/HW_lesson3/Program.cs
+++ HW_Lesson-3/HW_lesson3/Program.cs
@@ -2,63 +2,63 @@
 {
     private static void Main(string[] args)
     {
-        int a;
-        int b;
-        int result;
-        string operation;
+            double FirstNumber; // Первое число
+            double SecondNumber; // Второе число
+            double result; // Результат
+            string operation; // Выбор операции
 
-        Console.WriteLine("Программа, может выполнить арифметические вычисления с двумя числами.\n");
-
-       
-            Console.Write("Код операций:\n1 - Сложение\n2 - Вычитание\n3 - Возведение с степень\n4 - Умножение\n5 - Деление\n");
+            Console.WriteLine("Программа, может выполнить арифметические вычисления с двумя числами.\n");
+            Console.Write("Код операций:\n1 - Сложение\n2 - Вычитание\n3 - Возведение с степень\n4 - Умножение\n5 - Деление\n0 - Закончить выполенние программы\n");
             Console.Write("Выберите Вашу операцию:");
             operation = Console.ReadLine();
-            switch (operation)
+
+        switch (operation)
             {
                 case "1":
                     Console.Write("Введите первую переменную:");
-                    a = Convert.ToInt32(Console.ReadLine());
+                    FirstNumber = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Введите вторую переменную:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    result = a + b;
-                    Console.WriteLine($"Решение: {a} + {b} = {result}");
+                    SecondNumber = Convert.ToDouble(Console.ReadLine());
+                    result = FirstNumber + SecondNumber;
+                    Console.WriteLine($"Решение: {FirstNumber} + {SecondNumber} = {result}");
                     break;
                 case "2":
                     Console.Write("Введите первую переменную:");
-                    a = Convert.ToInt32(Console.ReadLine());
+                    FirstNumber = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Введите вторую переменную:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    result = a - b;
-                    Console.WriteLine($"Решение: {a} - {b} = {result}");
+                    SecondNumber = Convert.ToDouble(Console.ReadLine());
+                    result = FirstNumber - SecondNumber;
+                    Console.WriteLine($"Решение: {FirstNumber} - {SecondNumber} = {result}");
                     break;
                 case "3":
                     Console.Write("Введите первую переменную:");
-                    a = Convert.ToInt32(Console.ReadLine());
+                    FirstNumber = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Введите степень переменной:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    result = (int)Math.Pow(a, b);
-                    Console.WriteLine($"Решение: {a} + {b} = {result}");
+                    SecondNumber = Convert.ToDouble(Console.ReadLine());
+                    result = (int)Math.Pow(FirstNumber, SecondNumber);
+                    Console.WriteLine($"Решение: {FirstNumber}^{SecondNumber} = {result}");
                     break;
                 case "4":
                     Console.Write("Введите первую переменную:");
-                    a = Convert.ToInt32(Console.ReadLine());
+                    FirstNumber = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Введите вторую переменную:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    result = a * b;
-                    Console.WriteLine($"Решение: {a} * {b} = {result}");
+                    SecondNumber = Convert.ToDouble(Console.ReadLine());
+                    result = FirstNumber * SecondNumber;
+                    Console.WriteLine($"Решение: {FirstNumber} * {SecondNumber} = {result}");
                     break;
                 case "5":
                     Console.Write("Введите первую переменную:");
-                    a = Convert.ToInt32(Console.ReadLine());
+                    FirstNumber = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Введите вторую переменную:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    result = a / b;
-                    Console.WriteLine($"Решение: {a} / {b} = {result}");
+                    SecondNumber = Convert.ToDouble(Console.ReadLine());
+                    result = FirstNumber / SecondNumber;
+                    Console.WriteLine($"Решение: {FirstNumber} / {SecondNumber} = {result}");
+                    break;
+                case "0":
                     break;
                 default:
                     Console.WriteLine($"Вы ввели:{operation}. Такой команды не сущейтвует, введите коректный код операции:\n1 - Сложение\n2 - Вычитание\n3 - Возведение с степень\n");
                     break;
             }
-        Console.ReadLine();
     }
 }
