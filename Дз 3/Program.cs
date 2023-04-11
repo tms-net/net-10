1ac847163fa93e2182a2ff43b81a3056ae175553
YahorLaryonenka <129786109+YahorLaryonenka@users.noreply.github.com>
2023-04-11 15:31:52 +0300

    Add files via upload

---
+++ Дз 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+double a;
+double b;
+double result;
+char operations;
+
+Console.WriteLine("Ведите первое число");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ведите опервтор");
+operations = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Ведите второе чисто");
+b = Convert.ToDouble(Console.ReadLine());
+
+if (operations == '+')
+{
+    result = a + b;
+    Console.WriteLine(a + b = result);
+}
+else if (operations == '-') ;}
+{
+    Console.WriteLine(a - b = result); }
+
+else if (operations == '*') ;
+{ Console.WriteLine(a * b = result); }
+
+else if (operations == '/') ;
+{ Console.WtiteLine(a / b = result); }
+
+
+
+
