07ffb92cc7b218f4d6899fbe3056e7dafd6d74ed
Lexeyy <129971083+Lexeyy@users.noreply.github.com>
2023-04-11 23:17:47 +0300

    Add files via upload
    Adding homework-04

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Array taksks");
+
+int[] myArray = new int[] { 4, 22, 37, 9, 13, 40, 51 };
+
+int[] result = SolveArrayTask(myArray);
+
+Console.WriteLine(string.Join(", ", result));
+
+int[] SolveArrayTask(int[] nums)
+{
+    int[] reversedArray = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        reversedArray[i] = nums[nums.Length - 1 - i];
+    }
+
+    return reversedArray;
+}
