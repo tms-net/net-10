241b69077f66ecd80ee3404a0cf3d3783640fdf9
Dzmitry-Patapau <potapovdimitry@gmail.com>
2023-04-11 15:25:37 +0300

    Update project

--- homeworks/homework-03/PatapauHW3/Program.cs
+++ homeworks/homework-03/PatapauHW3/Program.cs
@@ -2,55 +2,61 @@
 {
     internal class Program
     {
-        private static bool work = true;
         static void Main(string[] args)
         {
-            //(выполняется пока не введина команда exit для завершения работы)
-            while (work)
-            {
-                DataInput();
-            }
-        }
-
-        private static void DataInput()
-        {
+            bool isRunning = true;
+            bool newNumbers;
             double firstNumber, secondNumber;
-            bool newNumber = false;
-            //Ввод первого числа
-            Console.WriteLine("Введите первое число");
-            while (!double.TryParse(Console.ReadLine(), out firstNumber))
+            string operation;
+ 
+            while (isRunning)
             {
-                Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода первого числа!");
-            }
-            //Ввод второго числа
-            Console.WriteLine("Введите второе число");
-            while (!double.TryParse(Console.ReadLine(), out secondNumber))
-            {
-                Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода второго числа!");
-            }
-            //Выбор операции над числами(выполняется пока не введина команда newNumber для ввода новых значений)
-            while (!newNumber)
-            {
-                //exit - выход из программы, newNumber - ввод новых значений чисел
-                Console.WriteLine("Выберите операцию над числами( + , - , / , * , % , ^ , exit - завершить работу, newNumber - ввод новых значений чисел)");
-                string operation = Console.ReadLine();
-
-                switch (operation)
+                newNumbers = false;
+                //Ввод первого числа
+                Console.WriteLine("Введите первое число");
+                while (!double.TryParse(Console.ReadLine(), out firstNumber))
                 {
-                    case "exit":
-                        work = false;
-                        newNumber = true;
-                        Console.WriteLine("Работа программы завершена. Нажмите любую клавишу для закрытия консоли.");
-                        break;
-                    case "newNumber":
-                        newNumber = true;
-                        break;
-                    default:
-                        Console.WriteLine("Результат операции. " + Calculate(operation, firstNumber, secondNumber));
-                        break;
+                    Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода первого числа!");
+                }
+                //Ввод второго числа
+                Console.WriteLine("Введите второе число");
+                while (!double.TryParse(Console.ReadLine(), out secondNumber))
+                {
+                    Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода второго числа!");
+                }
+                //Выбор операции над числами(выполняется пока не введина команда newNumber для ввода новых значений)
+                while (!newNumbers)
+                {
+                    //exit - выход из программы, newNumber - ввод новых значений чисел
+                    Console.WriteLine("Выберите операцию над числами( + , - , / , * , % , ^ , exit - завершить работу, newNumber - ввод новых значений чисел)");
+                    operation = Console.ReadLine();
+
+                    switch (operation)
+                    {
+                        case "exit":
+                            isRunning = false;
+                            newNumbers = true;
+                            Console.WriteLine("Работа программы завершена. Нажмите любую клавишу для закрытия консоли.");
+                            break;
+                        case "newNumber":
+                            newNumbers = true;
+                            break;
+                        case "+":
+                        case "-":
+                        case "/":
+                        case "*":
+                        case "^":
+                        case "%":
+                            Console.WriteLine("Результат операции. " + Calculate(operation, firstNumber, secondNumber));
+                            break;
+                        default:
+                            Console.WriteLine("Введена неизвестная операция! Попробуйте повторить попытку.");
+                            break;
+                    }
                 }
             }
         }
+
         //функция возвращающая значение после выполнения операции на числами
         private static string Calculate(string operation, double firstNumber, double secondNumber)
         {
@@ -62,7 +68,7 @@
                 "*" => $"{firstNumber * secondNumber}",
                 "%" => secondNumber == 0 ? "На ноль делить нельзя!" : $"{firstNumber % secondNumber}",
                 "^" => $"{Math.Pow(firstNumber, secondNumber)}",
-                _ => "Введина неизвестная операция!"
+                _ => "Введена неизвестная операция!"
             };
         }
     }
