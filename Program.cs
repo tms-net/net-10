6c7f33f4b107801baf43774d306d5a647959cb5b
YahorLaryonenka <129786109+YahorLaryonenka@users.noreply.github.com>
2023-04-11 15:30:54 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
-﻿long a = 3;
-long b = 3;
-Console.WriteLine($"{a} ^ {b} = {Math.Pow(a, b)}"); // Math.Pow()
+﻿using System.ComponentModel.Design;
+
+double a;
+double b;
+double result;
+char operations;
+
+Console.WriteLine("Ведите первое число");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ведите опервтор");
+operations = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Ведите второе чисто");
+b = Convert.ToDouble(Console.ReadLine());
+
+if (operations == '+')
+{
+    result = a + b;
+    Console.WriteLine(a + b = result);
+}
+else if (operations == '-') ;}
+{
+    Console.WriteLine(a - b = result); }
+
+else if (operations == '*') ;
+{ Console.WriteLine(a * b = result); }
+
+else if (operations == '/') ;
+{ Console.WtiteLine(a / b = result); }
+
+
+
+
