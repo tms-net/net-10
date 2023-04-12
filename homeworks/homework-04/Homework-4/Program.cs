d98f49103e4ab105b70e461b926641079a2545eb
Dzmitry-Patapau <potapovdimitry@gmail.com>
2023-04-12 17:05:12 +0300

    HW_4

--- homeworks/homework-04/Homework-4/Program.cs
+++ homeworks/homework-04/Homework-4/Program.cs
@@ -1,11 +1,120 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Array taksks");
 
-int[] myArray = new int[] { 4, 22, 37, 9, 13, 40, 51 };
+//Ввод массива
+uint arraySize;
+Console.WriteLine("Введите размерность массива:");
 
-int[] result = SolveArrayTask(myArray);
-
-int[] SolveArrayTask(int[] nums)
+while (!UInt32.TryParse(Console.ReadLine(), out arraySize))
 {
-    throw new NotImplementedException();
+    Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода размерности массива!");
 }
+
+int[] myArray = new int[arraySize];
+
+for (int n = 0; n < myArray.Length; n++)
+{
+    Console.WriteLine($"Введите элемента {n + 1} массива:");
+
+    while (!Int32.TryParse(Console.ReadLine(), out myArray[n]))
+    {
+        Console.WriteLine("Введен неверный формат данных! Повторите попытку ввод элемента массива!");
+    }
+}
+//Исходный массив
+Console.WriteLine("\nSource array:");
+DataOutput(myArray);
+
+Console.WriteLine("\nArrayReverse");
+DataOutput(ArrayReverse(myArray));
+
+Console.WriteLine("\nOddNumbers");
+DataOutput(OddNumbers(myArray));
+
+
+Console.WriteLine("\nEvenNumbers");
+DataOutput(EvenNumbers(myArray));
+
+
+
+
+//Вывод массивов в консоль
+void DataOutput(int[] arr)
+{
+    foreach (int i in arr)
+        Console.Write(i + " ");
+}
+
+
+//---------------------task1-----------------------------------------------//
+
+int[] ArrayReverse(int[] nums)
+{
+    int[] temp = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        temp[i] = nums[nums.Length - 1 - i];
+    }
+    return temp;
+}
+
+//--------------------task2---------------------------------------------------//
+
+int[] OddNumbers(int[] nums)
+{
+    int oddNum = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] % 2 != 0)
+        {
+            oddNum++;
+        }
+
+    }
+    int[] temp = new int[oddNum];
+    oddNum = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] % 2 != 0)
+        {
+            temp[oddNum] = nums[i];
+            oddNum++;
+        }
+    }
+    return temp;
+    //int oddNum = 0;
+    //int[] temp = new int[nums.Length];
+    //for (int i = 0; i < nums.Length; i++)
+    //{
+    //    if (nums[i] % 2 != 0)
+    //    {
+    //        temp[oddNum++] = nums[i];
+    //    }
+    //}
+}
+
+int[] EvenNumbers(int[] nums)
+{
+    int oddNum = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] % 2 == 0)
+        {
+            oddNum++;
+        }
+
+    }
+    int[] temp = new int[oddNum];
+    oddNum = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] % 2 == 0)
+        {
+            temp[oddNum] = nums[i];
+            oddNum++;
+        }
+    }
+    return temp;
+}
+
+//----------------------------------task3-----------------------------------------------------////
