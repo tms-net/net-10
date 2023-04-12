e058af3f80b4c66008afef1aa5f21e8b486c0dfd
PavelNTD <dunecpasha@yandex.ru>
2023-04-12 12:49:31 +0300

    Homework 4. Task 4. End

--- dunec homeworks/ConsoleAppWithArrays/ConsoleAppWithArrays/Program.cs
+++ dunec homeworks/ConsoleAppWithArrays/ConsoleAppWithArrays/Program.cs
@@ -48,7 +48,56 @@ internal class Program
         Console.WriteLine("\nRunning sum of the array:");
         PrintArray(operativeArrayOfNumbers);
 
+        //Fourth task
+        bool flagOfCorrectChoice = false;
+        while (!flagOfCorrectChoice)
+        {
+            string countOfStepsToMoveInArrayString = "";
+            int countOfStepsToMoveInArray = 0;
+            Console.WriteLine("\nInput count of steps to the right:");
+            try
+            {
+                countOfStepsToMoveInArrayString = Console.ReadLine();
+                if (int.TryParse(countOfStepsToMoveInArrayString, out countOfStepsToMoveInArray))
+                {
+                    if (countOfStepsToMoveInArray >= arrayOfNumbers.Length)
+                    {
+                        countOfStepsToMoveInArray = countOfStepsToMoveInArray % arrayOfNumbers.Length;
+                    }
+                    if (countOfStepsToMoveInArray != 0)
+                    {
+                        if (countOfStepsToMoveInArray > 0)
+                        {
+                            Array.Copy(arrayOfNumbers, arrayOfNumbers.Length - countOfStepsToMoveInArray, operativeArrayOfNumbers, 0, countOfStepsToMoveInArray);
+                            Array.Copy(arrayOfNumbers, 0, operativeArrayOfNumbers, countOfStepsToMoveInArray, arrayOfNumbers.Length - countOfStepsToMoveInArray);
 
+                            Console.WriteLine($"\nThe array has moved to {countOfStepsToMoveInArrayString} steps to the right:");
+                            PrintArray(operativeArrayOfNumbers);
+
+                            flagOfCorrectChoice = true;
+                        }
+                        else
+                        {
+                            Array.Copy(arrayOfNumbers, Math.Abs(countOfStepsToMoveInArray), operativeArrayOfNumbers, 0, arrayOfNumbers.Length + countOfStepsToMoveInArray);
+                            Array.Copy(arrayOfNumbers, 0, operativeArrayOfNumbers, arrayOfNumbers.Length + countOfStepsToMoveInArray, Math.Abs(countOfStepsToMoveInArray));
+
+                            Console.WriteLine($"\nThe array has moved to {countOfStepsToMoveInArrayString} steps to the left:");
+                            PrintArray(operativeArrayOfNumbers);
+
+                            flagOfCorrectChoice = true;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nInputed text can't be converted into a whole number or inputted number is bigger than 2,147,483,647");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nInputed text isn't correct: " + e.Message);
+            }               
+        }       
         Console.ReadLine();
     }
 
