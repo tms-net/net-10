004dea7bf5faf1637befa29dd2e5b2c735621e45
Alexander Pogosyan <alexanderpogosyan04@gmail.com>
2023-04-12 15:11:17 +0400

    Add new project and Add task 1 to branch

---
+++ hw-projects/ConsoleApp2(hw-4)/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp2_hw_4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1)Измените порядок массива на противоположный
+
+            Console.WriteLine("enter array length: ");
+            int arrLength = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arrLength];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10);
+                Console.Write(array[i]+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new String('-',50));
+            ArrayRevers(ref array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+" ");
+            }
+
+
+            //delay
+            Console.ReadKey();
+
+        }
+
+        static void ArrayRevers(ref int[] array)
+        {
+            int[] arr = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arr[i] = array[array.Length - i - 1];
+            }
+            array = arr;
+        }
+    }
+}
