154f1bd837e6b7a309bc3434609ae72fa9f2cf8e
PavelNTD <dunecpasha@yandex.ru>
2023-04-12 11:51:17 +0300

    Homowork 4. Task 1

---
+++ dunec homeworks/ConsoleAppWithArrays/ConsoleAppWithArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var rand = new Random();
+        double[] arrayOfNumbers = new double[rand.Next(3, 10)];
+        double[] operativeArrayOfNumbers = new double[arrayOfNumbers.Length];
+
+        for (int i = 0; i < arrayOfNumbers.Length; i++)
+        {
+            arrayOfNumbers[i] = Math.Round(rand.NextDouble() * 10, 5);
+        }
+        operativeArrayOfNumbers = (double[])arrayOfNumbers.Clone();
+
+        Console.WriteLine($"Created array of {arrayOfNumbers.Length} elements: ");
+        PrintArray(arrayOfNumbers);
+
+        Array.Reverse(operativeArrayOfNumbers);
+        Console.WriteLine("\nReversed array: ");
+        PrintArray(operativeArrayOfNumbers);
+
+        Console.ReadLine();
+    }
+
+    static void PrintArray(double[] array)
+    {
+        string outputArray = Convert.ToString(array[0]);
+        for (int i = 1; i < array.Length; i++)
+        {
+            outputArray = outputArray + " " + Convert.ToString(array[i]);
+        }  
+        Console.WriteLine(outputArray);
+    }
+}
