40c3f917abea5395306e0da5eb7a334c0b78b8e0
ManBanshee <klindyuk98@gmail.com>
2023-04-12 10:16:21 +0300

    Add HW4

--- homeworks/homework-04/Homework-4/Program.cs
+++ homeworks/homework-04/Homework-4/Program.cs
@@ -2,10 +2,21 @@
 Console.WriteLine("Array taksks");
 
 int[] myArray = new int[] { 4, 22, 37, 9, 13, 40, 51 };
+int k = 5;
 
-int[] result = SolveArrayTask(myArray);
+int[] result = SolveArrayTask(myArray, k);
 
-int[] SolveArrayTask(int[] nums)
+int[] SolveArrayTask(int[] nums, int k)
 {
-    throw new NotImplementedException();
+    for (int i = 0; i < k; i++)
+    {
+        int[] otherArray = new int[nums.Length];
+        int lastItem = nums[^1];
+        Array.Copy(nums, 0, otherArray, 1, otherArray.Length - 1);
+        otherArray[0] = lastItem;
+        nums = otherArray;
+        var output = string.Join(" ", nums);
+        Console.WriteLine(output);
+    }
+    return nums;
 }
