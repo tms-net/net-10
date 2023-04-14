76fe3430debfec6a6257073f77554d398ced372a
MenterCOX <danya.yagnish@mail.ru>
2023-04-14 16:23:21 +0300

    HW_4

--- homeworks/homework-04/Homework-4/Program.cs
+++ homeworks/homework-04/Homework-4/Program.cs
@@ -1,11 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Array taksks");
 
-int[] myArray = new int[] { 4, 22, 37, 9, 13, 40, 51 };
+Console.Write("Enter array length:");
 
-int[] result = SolveArrayTask(myArray);
+int[] myArray = new int[Int32.Parse(Console.ReadLine())];
 
-int[] SolveArrayTask(int[] nums)
+for(int i = 0; i < myArray.Length; i++)
 {
+    Console.Write($"Enter {i+1} element:");
+    myArray[i]=Int32.Parse(Console.ReadLine());
+}
+
+Console.Write("Enter shift:");
+
+int shift = Int32.Parse(Console.ReadLine());
+
+int[] result = SolveArrayTask(myArray,shift);
+
+int[] SolveArrayTask(int[] nums,int shift)
+{  
     throw new NotImplementedException();
 }
+
