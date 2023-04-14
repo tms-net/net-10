4eb0b9e0fa9c1381fdba7f2ba43d8b5543cb77f8
Dmitry Andrienko <d.andrienko@live.com>
2023-04-14 12:30:58 +0300

    Lesson 5 code

---
+++ homeworks/homework-04/Homework-4/ArrayExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Homework_4
+{
+    class MyArray : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    static class ArrayExtensions
+    {
+        internal static object[] ReverseToArray(this IEnumerable enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+
+            }
+
+            var enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current;
+
+                { 
+                }
+            }
+        }
+
+        internal static int[] ReverseArray(this int[] array) 
+        {
+            return array[^0..];
+        }
+    }
+}
