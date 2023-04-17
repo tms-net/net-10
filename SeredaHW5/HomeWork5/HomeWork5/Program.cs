a86527168cd26766b4c5af4fd8acd03c118f0ccc
ILya <ilyasereda9095@gmail.com>
2023-04-17 09:27:35 +0300

    Hw5

---
+++ SeredaHW5/HomeWork5/HomeWork5/Program.cs
@@ -0,0 +1,10 @@
+﻿
+abstract class Vehicle
+{
+    string Mark { get; set; }
+    string Model { get; set; }
+    string Year { get; set; }
+   protected abstract void Start();//без protected выбивает ошибку
+   protected abstract void Stop();
+}
+
