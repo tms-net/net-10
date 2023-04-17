2ad1de9a77207d7ae827fc3416c06cd5c87426a7
ILya <ilyasereda9095@gmail.com>
2023-04-17 11:07:03 +0300

    HW5

--- SeredaHW5/HomeWork5/HomeWork5/Program.cs
+++ SeredaHW5/HomeWork5/HomeWork5/Program.cs
@@ -1,10 +1,70 @@
 ﻿
-abstract class Vehicle
+public interface IMovable
 {
-    string Mark { get; set; }
-    string Model { get; set; }
-    string Year { get; set; }
-   protected abstract void Start();//без protected выбивает ошибку
-   protected abstract void Stop();
+    void Start();
+    void Stop();
 }
+
+public interface HaveEngine
+{
+    void OnEngine();
+    void OffEngine();
+}
+
+internal class Vehicle : IMovable, HaveEngine
+{
+    //private readonly int year
+    protected int CurrentSpeed;
+
+    public string Mark { get; set; }
+    public string Model { get; set; }
+    public string Year { get; set; }
+    public virtual void Start()
+    {
+        OnEngine();
+    }
+    public virtual void Stop()
+    {
+        OffEngine();
+    CurrentSpeed = 0;
+    }
+}
+
+//конкретные сущности
+public class Car: Vehicle 
+{
+    override void Start ()
+    {
+        CurrentSpeed = 100;
+    }
+}
+
+public class Truck: Vehicle
+{
+    override void Start()
+    {
+        CurrentSpeed = 10;
+    }
+
+
+}
+
+internal class Motorcycle: Vehicle
+{
+    private bool TripodEjected;
+    override void Start()
+    {
+        CurrentSpeed = 200;
+    }
+
+    override void Stop()
+    {
+        base.Stop();
+        TripodEjected = true;
+
+    }
+}
+
+
+
 
