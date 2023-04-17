52ee78479d28d80992b1f388232115903b96acf2
Молодой Дед <129785893+ZedDed0k@users.noreply.github.com>
2023-04-17 19:02:27 +0300

    Add files via upload

---
+++ HOMEWORK 5/HOMEWORK 5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+abstract class Vehicle
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string Year { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine("Запуск двигателя");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Остановка двигателя");
+    }
+
+    public abstract void Drive();
+}
+
+class Car : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Едем по дороге на автомобиле");
+    }
+}
+
+class Bicycle : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Катаемся на велосипеде");
+    }
+}
+
+class Motorcycle : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Катаемся на мотоцикле");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle car = new Car();
+        car.Make = "Нурлан точно";
+        car.Model = "не Сабуров";
+        car.Year = "2000";
+        car.Start();
+        car.Drive();
+        car.Stop();
+
+        Vehicle bicycle = new Bicycle();
+        bicycle.Make = "Создатель";
+        bicycle.Model = "Просто.... Модель";
+        bicycle.Year = "2001";
+        bicycle.Start();
+        bicycle.Drive();
+        bicycle.Stop();
+
+        Vehicle motorcycle = new Motorcycle();
+        motorcycle.Make = "Клоун";
+        motorcycle.Model = "Без понятия";
+        motorcycle.Year = "2002";
+        motorcycle.Start();
+        motorcycle.Drive();
+        motorcycle.Stop();
+
+        Console.ReadKey();
+    }
+}
+
