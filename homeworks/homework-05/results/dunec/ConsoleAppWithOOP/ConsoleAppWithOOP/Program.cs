71934d91f20489986d4abac221bf484b6adb2137
PavelNTD <dunecpasha@yandex.ru>
2023-04-19 16:57:45 +0300

    Homework 5. Reforged

---
+++ homeworks/homework-05/results/dunec/ConsoleAppWithOOP/ConsoleAppWithOOP/Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppWithOOP;
+using ConsoleAppWithOOP.Vehicles;
+using System.Reflection;
+using System.Xml.Linq;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var motorcycle = new Motorcycle(make: "Honda", model: "Rx21", 2017, 4.59);
+        var car = new Car(make: "BMW", model: "X4", 2005, 10.36);
+        var truck = new Truck(make: "KamAZ", model: "53501", 1985, 21.4);
+
+        Console.WriteLine("We're going to try use 3 type of transport:");
+        Console.WriteLine($"Motorcycle: {motorcycle.Make}-{motorcycle.Model} ({motorcycle.Year.ToString()});\nCar: {car.Make}-{car.Model} ({car.Year.ToString()});\nTruck: {truck.Make}-{truck.Model} ({truck.Year.ToString()}).");
+
+        Console.WriteLine($"\nTry to start {motorcycle.Make}-{motorcycle.Model} engine");
+        motorcycle.Start();
+        Console.WriteLine($"{motorcycle.Make}-{motorcycle.Model} start moving!");
+        
+
+        Console.WriteLine($"\nTry to start {car.Make}-{car.Model} engine");
+        car.Start();
+        Console.WriteLine($"{car.Make}-{car.Model} start moving!");
+        
+
+        Console.WriteLine($"\nTry to start {truck.Make}-{truck.Model} engine");
+        truck.Start();
+        Console.WriteLine($"{truck.Make}-{truck.Model} start moving!");
+        
+
+        Console.WriteLine("\nLet's check ours vehicle's speed:");
+        Console.WriteLine($"{motorcycle.Make}-{motorcycle.Model}: {motorcycle.GetCurrentSpeed().ToString()};\n{car.Make}-{car.Model}: {car.GetCurrentSpeed().ToString()};\n{truck.Make}-{truck.Model}: {truck.GetCurrentSpeed().ToString()}.");
+        
+        Console.WriteLine("\nWell, Now let's check: Could our vehicles stop on a 50 meter road?, It's time to stop!");
+
+        Random ran = new Random();
+        SetStatusOfRoad(ran.Next(1, 4), motorcycle);
+        if (motorcycle.SuccessfulStopOnDistance(50))
+        {
+            Console.WriteLine($"{motorcycle.Make}-{motorcycle.Model} was successfully stopped");
+        }
+        else
+        {
+            Console.WriteLine($"Oh no, there's the accident with {motorcycle.Make}-{motorcycle.Model}!");
+        }
+
+        SetStatusOfRoad(ran.Next(1, 4), car);
+        if (car.SuccessfulStopOnDistance(50))
+        {
+            Console.WriteLine($"{car.Make}-{car.Model} was successfully stopped");
+        }
+        else
+        {
+            Console.WriteLine($"Oh no, there's the accident with {car.Make}-{car.Model}!");
+        }
+
+        SetStatusOfRoad(ran.Next(1, 4), truck);
+        if (truck.SuccessfulStopOnDistance(50))
+        {
+            Console.WriteLine($"{truck.Make}-{truck.Model} was successfully stopped");
+        }
+        else
+        {
+            Console.WriteLine($"Oh no, there's the accident with {truck.Make}-{truck.Model}!");
+        }
+
+        Console.ReadLine();
+    }
+
+    public static void SetStatusOfRoad(int indexOfStatus, Vehicle vehicle)
+    { 
+        switch (indexOfStatus)
+        {
+            case 1:
+                {
+                    Console.WriteLine($"\n{vehicle.Make}-{vehicle.Model} are moving on dry asphalt");
+                    if (!vehicle.SetCoefficientOfFriction(0.8))
+                    {
+                        Console.WriteLine($"Encorrect coefficient of friction for {vehicle.Make}-{vehicle.Model}");
+                    }                    
+                }                
+                break;
+            case 2:
+                {
+                    Console.WriteLine($"\n{vehicle.Make}-{vehicle.Model} are moving on wet asphalt");
+                    if (!vehicle.SetCoefficientOfFriction(0.4))
+                    {
+                        Console.WriteLine($"Encorrect coefficient of friction for {vehicle.Make}-{vehicle.Model}");
+                    }                   
+                }
+                break;
+            case 3:
+                {
+                    Console.WriteLine($"\n{vehicle.Make}-{vehicle.Model} are moving on ice");
+                    if (!vehicle.SetCoefficientOfFriction(0.1))
+                    {
+                        Console.WriteLine($"Encorrect coefficient of friction for {vehicle.Make}-{vehicle.Model}");
+                    }                    
+                }
+                break;
+        }
+    }
+}
---
+++ homeworks/homework-05/results/dunec/ConsoleAppWithOOP/ConsoleAppWithOOP/Vehicles/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithOOP.Vehicles
+{
+    internal class Car : Vehicle, IHaveEngine
+    {
+        public Car(string make, string model, int year, double weigh) : base(make, model, year, weigh)
+        {
+
+        }
+
+        public bool SuccesStart()
+        {
+            Random ran = new Random();
+            if (ran.Next(1, 20) > 10)
+            { return true; }
+            else
+            { return false; }
+        }
+        /*
+        public bool SuccesStop()
+        {
+            Random ran = new Random();
+            if (ran.Next(1, 5) > 3)
+            { return true; }
+            else
+            { return false; }
+        }//*/
+
+        override public void Start()
+        {
+            while (!SuccesStart())
+            {
+                Console.WriteLine("Failed");
+            }
+            CurrentSpeed = 69;
+        }
+    }
+}
