f12564680ab651e65c97b9ad19e88c23a2f9987d
nastya <nastiusha_tom@icloud.com>
2023-04-25 18:51:26 +0300

    Added console project

---
+++ homeworks/homework-04/results/tomchuk/Homework-4/Homework-4/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        int[] myArray = new int[] { 4, 22, 37, 9, 13, 40, 51 };
+
+        int[] result = SolveArrayTask(myArray);
+
+        foreach (int i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    static int[] SolveArrayTask(int[] nums)
+    {
+        Array.Reverse(nums);
+        return nums;
+    }
+
+}
