94f40b3a721fc0ffac542f9f9a1c88f212b5dc55
Dzmitry-Patapau <potapovdimitry@gmail.com>
2023-05-01 00:07:55 +0300

    add project HW7

---
+++ homeworks/homework-07/results/Patapau/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Patapau
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UnitStateManager<CanonState> unitStateManager = new UnitStateManager<CanonState>(
+                (CanonState.AwaitingTarget, CanonState.Aiming),
+                (CanonState.Aiming, CanonState.Attack),
+                (CanonState.Attack, CanonState.AwaitingTarget));
+            unitStateManager.AllowTransition(CanonState.Aiming, CanonState.AwaitingTarget);
+            try
+            {
+                unitStateManager.DeletePermissionTransition(CanonState.Aiming, CanonState.AwaitingTarget);
+                unitStateManager.MoveTo(CanonState.Attack);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+    }
+
+}
+
---
+++ homeworks/homework-07/results/Patapau/UnitStateManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Security;
+
+namespace Patapau
+{
+    /// <summary>
+    /// Автомат (машина) состояний для юнитов
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    internal class UnitStateManager<TState> where TState : struct, Enum
+    {
+
+        /// <summary>
+        /// Текущее состояние
+        /// </summary>
+        public TState Current { get; private set; }
+
+        private readonly Dictionary<TState, HashSet<TState>> dictionary;
+
+        public UnitStateManager(params (TState fromState, TState toState)[] states)
+        {
+            dictionary = new Dictionary<TState, HashSet<TState>>();
+
+            foreach (var state in states)
+            {
+                if(!dictionary.ContainsKey(state.fromState))
+                {
+                    dictionary.Add(state.fromState, new HashSet<TState>(){state.toState});
+                }
+                else
+                {
+                    dictionary[state.fromState].Add(state.toState);
+                }
+            }
+            Current = default;
+        }
+
+
+        /// <summary>
+        /// Настройка переходов между состояниями (добавление разрешений)
+        /// </summary>
+        /// <param name="fromState">Исходное состояние</param>
+        /// <param name="toState">Последующее состояние</param>
+        public void AllowTransition(TState fromState, TState toState)
+        {
+            if (!dictionary.ContainsKey(fromState))
+            {
+                dictionary.Add(fromState, new HashSet<TState>() { toState });
+            }
+            else
+            {
+                dictionary[fromState].Add(toState);
+            }
+        }
+
+
+        /// <summary>
+        /// Настройка переходов между состояниями (удаление разрешений)
+        /// </summary>
+        /// <param name="fromState">Исходное состояние</param>
+        /// <param name="toState">Последующее состояние</param>
+        public void DeletePermissionTransition(TState fromState, TState toState)
+        {
+            if (dictionary.ContainsKey(fromState) && dictionary[fromState].Contains(toState))
+            {
+                if (dictionary[fromState].Count<2)
+                {
+                    dictionary.Remove(fromState);
+                }
+                else
+                {
+                    dictionary[fromState].Remove(toState);
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"Transition from state {Current} to state {toState} was not detected.");
+            }
+        }
+
+
+        /// <summary>
+        /// Осуществить переход в состояние
+        /// </summary>
+        /// <param name="toState">Конечное состояние</param>
+        public void MoveTo(TState toState)
+        {
+
+            if (dictionary.ContainsKey(Current) && dictionary[Current].Contains(toState))
+            {
+                Current = toState;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Transition not allowed from {Current} to {toState} state.");
+            }
+
+        }
+
+    }
+
+}
+
