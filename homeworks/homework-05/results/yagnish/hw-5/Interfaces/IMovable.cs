ed90b1244ee36c6c2ea24bc2e30b90e97273d170
MenterCOX <danya.yagnish@mail.ru>
2023-04-30 16:29:12 +0300

    hw-5

---
+++ homeworks/homework-05/results/yagnish/hw-5/Interfaces/IMovable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_5.Interfaces
+{
+    internal interface IMovable
+    {
+        public void StartMovement(int speed);
+        public void StopMovement();
+        public void UpdateSpeed(int newSpeed);
+    }
+}
---
+++ homeworks/homework-05/results/yagnish/hw-5/Vehicles/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_5.Vehicles
+{
+    internal class Car : Base
+    {
+        readonly protected int _maxPass;
+        public Car(string year, string model, string mark, string maxSpeed, int maxPass):base(year, model, mark, maxSpeed)
+        {
+           _maxPass = maxPass;
+        }
+    }
+}
