9a49accc1681bb8a514d8f94651097c2d5f2abfc
gogurets <129603830+gogurets@users.noreply.github.com>
2023-05-01 18:46:00 +0300

    Add files via upload

---
+++ homeworks/homework-07/Zhdanov hw7/Zhdanov hw7/States.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhdanov_hw7
+{
+ 
+     ///<summary>
+     /// Cостояниt самолета
+     /// </summary>
+     
+    internal enum PlaneState
+    {
+        TakeOff,
+        Land,
+        Crash,
+        Refuel
+    }
+
+    ///<summary>
+    /// Состояния пушки
+    ///</summary>
+    internal enum CanonState
+    {
+        Awaiting,
+        Aim,
+        Atack        
+    }
+    internal enum TankState
+    {
+        Awaiting,
+        Moving,
+        Atack,
+        Defence
+    }
+}  
