819ef118fdf2dd2e9b22a4e9d7cae5fee9f32dbf
PavelNTD <dunecpasha@yandex.ru>
2023-05-02 11:33:02 +0300

    Homework 8. Beginning

--- homeworks/homework-07/samples/StarDefence/Program.cs
+++ homeworks/homework-07/samples/StarDefence/Program.cs
@@ -4,51 +4,56 @@ using StarDefenceUnits;
 
 
 
-var disp = new DisposableClass();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var disp = new DisposableClass();
 
-Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
 
-var canonState = CanonState.AwaitingTarget;
+        var canonState = CanonState.AwaitingTarget;
 
-var intValue = (int)canonState;
+        var intValue = (int)canonState;
 
-canonState = (CanonState)intValue;
+        canonState = (CanonState)intValue;
 
-// canon
+        // canon
 
-// state machine (конечный автомат, машина состояний)
+        // state machine (конечный автомат, машина состояний)
 
-// canon -> state machine
+        // canon -> state machine
 
-var canonStateManager = new UnitStateManager<CanonState>(
-    //new[] { CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/ },
-    //new[] { CanonState.Aiming /*0*/, CanonState.Atack /*1*/ },
-    //new[] { CanonState.Atack, CanonState.AwaitingTarget });
+        var canonStateManager = new UnitStateManager<CanonState>(
+            //new[] { CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/ },
+            //new[] { CanonState.Aiming /*0*/, CanonState.Atack /*1*/ },
+            //new[] { CanonState.Atack, CanonState.AwaitingTarget });
 
-    //new Tuple<CanonState, CanonState>(CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/),
-    //new Tuple<CanonState, CanonState>(CanonState.Aiming /*0*/, CanonState.Atack /*1*/),
-    //new Tuple<CanonState, CanonState>(CanonState.Atack, CanonState.AwaitingTarget ));
+            //new Tuple<CanonState, CanonState>(CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/),
+            //new Tuple<CanonState, CanonState>(CanonState.Aiming /*0*/, CanonState.Atack /*1*/),
+            //new Tuple<CanonState, CanonState>(CanonState.Atack, CanonState.AwaitingTarget ));
 
-//new []
-//{
-//    new [] { CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/ },
-//    new [] { CanonState.Aiming /*0*/, CanonState.Atack /*1*/ },
-//    new [] {CanonState.Atack, CanonState.AwaitingTarget }
-//} 
+            //new []
+            //{
+            //    new [] { CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/ },
+            //    new [] { CanonState.Aiming /*0*/, CanonState.Atack /*1*/ },
+            //    new [] {CanonState.Atack, CanonState.AwaitingTarget }
+            //} 
 
-    (from: CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/), //0
-    (CanonState.Aiming /*0*/, to: CanonState.Atack /*1*/), // 1
-    (CanonState.Aiming /*0*/, CanonState.AwaitingTarget /*1*/), //2
-    (CanonState.Atack /*0*/, CanonState.AwaitingTarget /*1*/) // 3
-    );
+            (from: CanonState.AwaitingTarget /*0*/, CanonState.Aiming /*1*/), //0
+            (CanonState.Aiming /*0*/, to: CanonState.Atack /*1*/), // 1
+            (CanonState.Aiming /*0*/, CanonState.AwaitingTarget /*1*/), //2
+            (CanonState.Atack /*0*/, CanonState.AwaitingTarget /*1*/) // 3
+            );
 
-// destroyer -> state machine
+        // destroyer -> state machine
 
-var destroyerManager = new UnitStateManager<DestroyerState>();
+        var destroyerManager = new UnitStateManager<DestroyerState>();
 
-// tank -> state machine
+        // tank -> state machine
 
-// получить команду -> нужное состояние
+        // получить команду -> нужное состояние
 
-canonStateManager.MoveTo(CanonState.Atack);
-
+        canonStateManager.MoveTo(CanonState.Atack);
+    }
+}
---
+++ homeworks/homework-08/results/dunec/Student Manager (ConsoleApp)/Student Manager (ConsoleApp)/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Manager__ConsoleApp_
+{
+    internal class Student
+    {
+        protected string ID { get; }
+        protected string StudentName { get; set; }
+        protected int Age { get; set; }
+        protected string Gender { get; set;}
+
+        public Student(string studentName, int age, string gender) {
+            ID = Guid.NewGuid().ToString();
+            StudentName = studentName;
+            Age = age;
+            Gender = gender;
+        }
+
+    }
+}
