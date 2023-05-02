cfcb72556c6cc36d93d5c4921a02b8567f2fa04b
Dzmitry-Patapau <potapovdimitry@gmail.com>
2023-05-02 22:51:35 +0300

    update HW8

---
+++ homeworks/homework-08/results/Patapau/Program.cs
@@ -0,0 +1,159 @@
+﻿using StudentManagerLibrary;
+
+namespace Patapau
+
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            StudentManager studentManager = new StudentManager();
+            studentManager.AddStudent("Дима", 15, 'М', 9);
+            studentManager.AddStudent("Настя", 15, 'Ж', 10);
+
+
+            bool isRunning = true;
+            string nameConsole;
+            char genderConsole;
+            byte ageConsole, gradeConsole;
+            int idConsole;
+            Student student;
+            Console.WriteLine("Добро пожаловать в программу StudentManager.");
+            while (isRunning)
+            {
+                Console.WriteLine("Выберите доступное действие:\n" +
+                    " 1 - Добавить студента\n" +
+                    " 2 - Редактировать студента\n" +
+                    " 3 - Удалить студента\n" +
+                    " 4 - Поиск студента по ID\n" +
+                    " 5 - Поиск студента по имени\n" +
+                    " 6 - Список студентов\n" +
+                    " 7 - Завершение работы.\n");
+                var operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "1":
+                        Console.Clear();
+                        EnterDataStudent();
+                        studentManager.AddStudent(nameConsole, ageConsole, genderConsole, gradeConsole);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Введите ID студента для редактирования");
+                        EnterStudentID();
+                        if (studentManager.CheckIdStudentExistence(idConsole))
+                        {
+                            EnterDataStudent();
+                            studentManager.EditStudent(idConsole, nameConsole, ageConsole, genderConsole, gradeConsole);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не найдено совпадений в БД!\n");
+                        }
+                        break;
+                   
+                    
+                    
+                    
+                    
+                    
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine("Введите ID студента для удаления");
+                        EnterStudentID();
+                        studentManager.RemoveStudent(idConsole);
+                        break;
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Введите ID студента для поиска");
+                        EnterStudentID();
+                        student = studentManager.SearchByID(idConsole);
+                        if (student == null)
+                            Console.WriteLine("Не найдено совпадений в БД!\n");
+                        Console.WriteLine(student);
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Введите имя студента для поиска");
+                        student = studentManager.SearchByName(Console.ReadLine());
+                        if (student == null)
+                            Console.WriteLine("Не найдено совпадений в БД!\n");
+                        Console.WriteLine(student);
+                        break;
+                    case "6":
+                        Console.Clear();
+                        var students = studentManager.GetStudents();
+                        if (students.Count == 0)
+                            Console.WriteLine("Нет студентов в БД! Необходимо добавить студентов!\n");
+                        foreach (var stud in students)
+                        {
+                            Console.WriteLine(stud);
+                        }
+                        break;
+                    case "7":
+                        Console.Clear();
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Неизвестная команда! Повторите попытку!\n");
+                        break;
+                }
+            }
+
+
+
+
+
+
+
+
+            // Ввод данных студента в консоль (nameConsole, ageConsole, genderConsole, gradeConsole)
+            void EnterDataStudent()
+            {
+                Console.WriteLine("Введите имя студента");
+                nameConsole = Console.ReadLine();
+                Console.WriteLine("Введите возраст студента");
+                while (!byte.TryParse(Console.ReadLine(), out ageConsole))
+                {
+                    Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода возраста студента!");
+                }
+                Console.WriteLine("Введите пол студента М или Ж");
+                while (!char.TryParse(Console.ReadLine(), out genderConsole) || (genderConsole != 'Ж' && genderConsole != 'М'))
+                {
+                    Console.WriteLine("Некорректный ввод, попробуйте снова:");
+                }
+                Console.WriteLine("Введите оценку");
+                while (!byte.TryParse(Console.ReadLine(), out gradeConsole))
+                {
+                    Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода возраста студента!");
+                }
+            }
+            //Ввод ID студента
+            void EnterStudentID()
+            {
+                while (!int.TryParse(Console.ReadLine(), out idConsole))
+                {
+                    Console.WriteLine("Введен неверный формат данных! Повторите попытку ввода ID студента!");
+                }
+
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ homeworks/homework-08/results/StudentManagerLibrary/StudentManager.cs
@@ -0,0 +1,87 @@
+﻿namespace StudentManagerLibrary
+{
+    public class StudentManager
+    {
+        private int counterIdStudent = 1;
+
+        private readonly List<Student> students;
+
+        public StudentManager()
+        {
+            students = new List<Student>();
+        }
+
+        public void AddStudent(string name, byte age, char gender, byte grade)
+        {
+            Student student = new Student(counterIdStudent, name, age, gender, grade);
+            students.Add(student);
+            counterIdStudent++;
+        }
+
+        public void EditStudent(int id, string name, byte age, char gender, byte grade)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            student.Name = name;
+            student.Age = age;
+            student.Gender = gender;
+            student.Grade = grade;
+        }
+
+        /// <summary>
+        /// Удаление студента по ID
+        /// </summary>
+        /// <param name="studentID">ID студента</param>
+        /// <returns>При успешном удалении возвращается true, иначе false.</returns>
+        public void RemoveStudent(int studentID)
+        {
+            var student = students.FirstOrDefault(student => student.Id == studentID);
+            if (student != null)
+            {
+                students.Remove(student);
+            }
+        }
+
+        /// <summary>
+        /// Поиск студента по имени. 
+        /// </summary>
+        /// <param name="name">Критерий поиска</param>
+        /// <returns>Возвращает копию обьекта при успехе иначе null</returns>
+        public Student SearchByName(string name)
+        {
+            var student = students.FirstOrDefault(student => student.Name == name);
+            if (student != null)
+                return new Student(student.Id, student.Name, student.Age, student.Gender, student.Grade);
+            return null;
+        }
+
+        /// <summary>
+        /// Поиск студента по ID студента. 
+        /// </summary>
+        /// <param name="ID">Критерий поиска</param>
+        /// <returns>Возвращает копию обьекта при успехе иначе null</returns>
+        public Student SearchByID(int ID)
+        {
+            var student = students.FirstOrDefault(student => student.Id == ID);
+            if (student != null)
+                return new Student(student.Id, student.Name, student.Age, student.Gender, student.Grade);
+            return null;
+        }
+        /// <summary>
+        /// Возвращает копию списока студентов 
+        /// </summary>
+        /// <returns>Возвращает копию списока студентов </returns>
+        public List<Student> GetStudents()
+        {
+            List<Student> CopyList = new List<Student>();
+            foreach( var student in students)
+                CopyList.Add(new Student(student.Id, student.Name, student.Age, student.Gender, student.Grade));
+            return CopyList;
+        }
+        public bool CheckIdStudentExistence(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            return student != null;
+        }
+
+    }
+}
