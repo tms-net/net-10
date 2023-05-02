aadafd5c7074e03f6da6110c3767ad7e56935694
Danila Vouk <dalalaler@gmail.com>
2023-05-02 23:55:24 +0300

    Added homework 8

---
+++ homeworks/homework-08/results/vouk/Homework8/Homework8/Managers/StudentManager.cs
@@ -0,0 +1,47 @@
+﻿using StudentsLibrary;
+
+namespace Homework8.Managers
+{
+    public  class StudentManager
+    {
+        private  Dictionary<string, List<Student>> _groupsWithStudents = new();
+
+        public  void AddNewGroup(string groupName) => _groupsWithStudents.Add(groupName, new List<Student>());
+
+        public  void RemoveGroup(string groupName) => _groupsWithStudents.Remove(groupName);
+
+        public  void AddStudentToGroup(string groupName, Student student) => _groupsWithStudents[groupName].Add(student);
+
+        public  void RemoveStudentFromGroup(Guid studentId)
+        {
+            foreach (var groupWithStudent in _groupsWithStudents)
+            {
+                groupWithStudent.Value.RemoveAll(st => st.Id == studentId);
+            }
+        }
+
+        public  Student GetUserByFirstName(string firstName) =>
+            _groupsWithStudents.SelectMany(s => s.Value).FirstOrDefault(s => s.FirstName == firstName);
+
+        public  Student GetUserByLastName(string lastName) =>
+            _groupsWithStudents.SelectMany(s => s.Value).FirstOrDefault(s => s.LastName == lastName);
+
+        public  void BackupStudentChanges(Guid studentId) =>
+            _groupsWithStudents.SelectMany(s => s.Value).FirstOrDefault(s => s.Id == studentId).BackupChanges();
+
+        public  void ChangeStudentData(Guid studentId,
+                                             string? firstName = null,
+                                             string? lastName = null,
+                                             DateOnly? birthDate = null,
+                                             string? faculty = null,
+                                             string? group = null) =>
+            _groupsWithStudents.SelectMany(s => s.Value)
+                               .FirstOrDefault(s => s.Id == studentId)
+                               .ChangeStudentData(
+                                   firstName,
+                                   lastName,
+                                   birthDate,
+                                   faculty,
+                                   group);
+    }
+}
---
+++ homeworks/homework-08/results/vouk/StudentsLibrary/StudentsLibrary/Student.cs
@@ -0,0 +1,53 @@
+﻿namespace StudentsLibrary
+{
+    public class Student
+    {
+        public string? FirstName { get; private set; }
+        public string? LastName { get; private set; }
+        public DateOnly? BirthDate { get; private set; }
+        public string? Faculty { get; private set; }
+        public string? Group { get; private set; }
+        public Guid Id { get; private set; }
+
+        private Student _previousStudentState;
+
+        public Student(string? firstName, string? lastName, DateOnly birthDate, string? faculty, string? group)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Faculty = faculty;
+            Group = group;
+            Id = Guid.NewGuid();
+        }
+
+        private Student(Student previousStudentState)
+        {
+            FirstName = previousStudentState.FirstName;
+            LastName = previousStudentState.LastName;
+            BirthDate = previousStudentState.BirthDate;
+            Faculty = previousStudentState.Faculty;
+            Group = previousStudentState.Group;
+            Id = previousStudentState.Id;
+        }
+
+        public void ChangeStudentData(string? firstName = null, string? lastName = null, DateOnly? birthDate = null, string? faculty = null, string? group = null)
+        {
+            _previousStudentState = new Student(this);
+            FirstName = firstName ?? FirstName;
+            LastName = lastName ?? LastName;
+            BirthDate = birthDate ?? BirthDate;
+            Faculty = faculty ?? Faculty;
+            Group = group ?? Group;
+        }
+        public void BackupChanges()
+        {
+            FirstName = _previousStudentState.FirstName;
+            LastName = _previousStudentState.LastName;
+            BirthDate = _previousStudentState.BirthDate;
+            Faculty = _previousStudentState.Faculty;
+            Group = _previousStudentState.Group;
+            _previousStudentState = new Student(this);
+        }
+    }
+}
