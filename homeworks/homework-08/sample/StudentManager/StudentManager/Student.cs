29f80a6416f0b5dd9d0bf9e2d8f421b1ddcce67a
Dmitry Andrienko <d.andrienko@live.com>
2023-05-08 00:12:55 +0300

    Add student manager sample

---
+++ homeworks/homework-08/sample/StudentManager/StudentManager/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace StudentManagerLibrary
+{
+    public enum Gender
+    {
+        Male,
+        Femail
+    }
+
+    public class Student
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public Gender Gender { get; set; }
+        public byte Grade { get; set; }
+
+        public Student(int id, string name, byte age, Gender gender, byte grade)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Grade = grade;
+            Id = id;
+        }
+
+        public override string ToString()
+        {
+            return $"ID студента - {Id}; Имя студента - {Name}; Возраст студента - {Age}; Пол студента - {Gender}; Оценка студента - {Grade}.\n";
+        }
+    }
+}
+    
