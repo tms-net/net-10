a177742de1f9bd31cd8af0f49d538b51f0eeedf6
Dzmitry Andryienka <dandryienka@microsoft.com>
2023-05-08 12:59:48 +0300

    Add hw 9

---
+++ homeworks/homework-09/sample/ATM/ATM/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Banking;
+
+Console.WriteLine("Hello, World!");
+
+var random = new Random();
+var atm = new ATM(10000);
+atm.TransactionCompleted += (sender, args) =>
+{
+    Console.WriteLine($"Transaction completed: {args.BalanceAfter}");
+};
+
+for (int i = 0; i < 10; i++)
+{
+    var client = new ATMClient(atm);
+
+    DoRandomActions(client);
+}
+
+// TODO: Реализуйте логику просмотра баланса пользователем
+
+void DoRandomActions(ATMClient client)
+{
+    var actions = new[] { client.Withdraw, client.TopUp };
+
+    client.InsertCard(random.Next(100, 10000));
+
+    var numberOfActions = random.Next(10);
+    for (int i = 0; i < numberOfActions; i++)
+    {
+        actions[random.Next(0,2)](random.Next(10, 1000));
+    }
+}
