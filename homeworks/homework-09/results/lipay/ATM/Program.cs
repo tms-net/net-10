81beaeef717083f2bbbc19cd77355dd7ca6544b2
Evgeny Lipay <lipay51131@mail.ru>
2023-05-09 22:00:46 +0300

    Add hw-9 files

---
+++ homeworks/homework-09/results/lipay/ATM/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Banking;
+
+
+
+var random = new Random();
+var atm = new ATM(10000);
+atm.TransactionCompleted += (sender, args) =>
+{
+    Console.WriteLine($"Transaction completed: {args.BalanceAfter}");
+};
+
+
+for (int i = 0; i < 10; i++)
+{
+    var client = new ATMClient(atm);
+
+    DoRandomActions(client);
+
+    Console.WriteLine($"Current balance:{client.ViewBalance()}"); 
+}
+
+
+Console.ReadLine();
+void DoRandomActions(ATMClient client)
+{
+    var actions = new[] { client.Withdraw, client.TopUp };
+
+    client.InsertCard(random.Next(100, 10000));
+
+    var numberOfActions = random.Next(10);
+    for (int i = 0; i < numberOfActions; i++)
+    {
+        actions[random.Next(0,2)](random.Next(10, 1000));
+    }
+}
