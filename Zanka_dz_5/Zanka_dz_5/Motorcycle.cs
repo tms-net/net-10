27e8c8b3369f434f75d5b0cdaef650938770d71a
dfcef5 <129785854+dfcef5@users.noreply.github.com>
2023-05-09 19:13:46 +0300

    Zanka_dz_5

---
+++ Zanka_dz_5/Zanka_dz_5/Motorcycle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zanka_dz_5
+{
+    internal class Motorcycle : Vehicle
+    {
+        bool _tripodEjected;
+        public override void Start()
+        {
+            _CurrentSpeed = 25;
+            throw new NotImplementedException();
+        }
+        public override void UpdateSpeed()
+        {
+            _CurrentSpeed = 50;
+            throw new NotImplementedException();
+        }
+        public override void Stop()
+        {
+            base.Stop();
+            _tripodEjected = true;
+        }
+    }
+}
---
+++ Zanka_dz_5/Zanka_dz_5/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zanka_dz_5
+{
+    public interface IVehicle
+    {
+        void Start();
+        void Stop();
+    }
+    abstract class Vehicle : IVehicle
+    {
+        protected int _CurrentSpeed;
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public  int? Year { get; set; }
+        public int MaxSpeed { get; set; }
+        public int Maxpeople { get; set; }
+        public abstract void Start();
+        public virtual void Stop()
+        {
+            _CurrentSpeed = 0;
+        }
+        public abstract void UpdateSpeed();
+
+    }
+}
