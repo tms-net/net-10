cce5f9f585ccab9aed06911028d32897e6391020
Nikita <mr.nikitakachura@gmail.com>
2023-05-10 21:08:29 +0200

    Add progress made on the lesson

---
+++ homeworks/homework-10/sample/TradingApp/TradingApp/IOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TradingApp
+{
+	public interface IOrder
+	{
+		public void MakeOrderMarket(string symbol,
+			int quantity,
+			decimal price);
+
+        public void MakeOrderPrice(string symbol,
+            int quantity,
+            decimal price);
+
+		public bool CancelOrder();
+    }
+}
+
---
+++ homeworks/homework-10/sample/TradingApp/TradingApp/SellOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TradingApp
+{
+	public class SellOrder : IOrder
+	{
+        public void MakeOrderMarket(string symbol, int quantity, decimal price)
+        {
+
+        }
+
+        public void MakeOrderPrice(string symbol, int quantity, decimal price)
+        {
+
+        }
+
+        public bool CancelOrder()
+        {
+
+        }
+    }
+}
+
