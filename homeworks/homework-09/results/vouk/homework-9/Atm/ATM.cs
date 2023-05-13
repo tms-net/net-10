2ba25d3d1c7f05d404f7de227d949feb68b703de
Danila Vouk <dalalaler@gmail.com>
2023-05-13 23:50:56 +0300

    Added first steps

---
+++ homeworks/homework-09/results/vouk/homework-9/Atm/ATM.cs
@@ -0,0 +1,61 @@
+﻿namespace Banking
+{
+    internal class ATM
+    {
+        private long _cacheAmount;
+
+        public event EventHandler<TransactionEventArgs> TransactionCompleted;
+
+        public ATM(long cashAmount)
+        {
+            _cacheAmount = cashAmount;
+        }
+
+        public void WithdrawMoney(long amount)
+        {
+            var args = new TransactionEventArgs();
+
+            if (_cacheAmount >= amount)
+            {
+                args.TransactionStatus = TransactionStatus.Succeeded;
+                args.BalanceBefore = _cacheAmount;
+
+                _cacheAmount -= amount;
+
+                args.BalanceAfter = _cacheAmount;
+            }
+            else
+            {
+                args.TransactionStatus = TransactionStatus.Failed;
+                args.TransactionError = "Not enough money";
+            }
+
+            if (TransactionCompleted != null)
+            {
+                TransactionCompleted(this, args);
+            }
+        }
+
+        public void TopUpMoney(long amount)
+        {
+            // TODO: Реализовать пополнение средств
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum TransactionStatus
+    {
+        Undefuned,
+        Succeeded,
+        Failed
+    }
+
+    public class TransactionEventArgs
+    {
+        public string TransactionError { get; set; }
+        public TransactionStatus TransactionStatus { get; set; }
+        public long BalanceAfter { get; set; }
+        public long BalanceBefore { get; set; }
+    }
+}
