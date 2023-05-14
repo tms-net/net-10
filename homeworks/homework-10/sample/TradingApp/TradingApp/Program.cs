04f3d6f6a819d96b459de467798d01823040be8c
Evgeny Lipay <lipay51131@mail.ru>
2023-05-14 18:29:06 +0300

    Add new code v1

--- homeworks/homework-10/sample/TradingApp/TradingApp/Program.cs
+++ homeworks/homework-10/sample/TradingApp/TradingApp/Program.cs
@@ -1,18 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TradingApp;
 
-Console.WriteLine("Hello, World!");
 
-// delegate bool InfoUpdated(SymbolInfo symbolInfo);
 
-// UI - Павел Дунец
-// Основной символ (тикер) -> ввод пользователя
-// Текущее значение цены
-// Информация по символу
+internal class Program
+{
+    private static void Main(string[] args)
+    {
 
-// Сопутствующие символы
-// Значение
-// Изменение (↑ | ↓)
+        //Dictionary<string, Dictionary<int, decimal>> MarketCap = new Dictionary<string, Dictionary<int, decimal>>();
+        //DateTime dateTime = new DateTime();
+        //TimeSpan time = TimeSpan.Zero;
+        //Dictionary<int, decimal> companyInfo = new Dictionary<int, decimal>() { [1] = 308 };
+        //MarketCap.Add("MSFT", companyInfo);
+        //var serch = MarketCap["MSFT"][1];
+        //Console.WriteLine(serch);
 
-// Возможность изменения частоты InfoUpdated += (Handler)
-// Функции
+        //Console.ReadLine();
+        //Console.ReadLine();
+        //SymbolInfo MyClass = new SymbolInfo();
+        //MyClass.CurrentInfo += MyClass.GetCurrentInfo;
+        //var period = TimeSpan.FromHours(1);
+        //var income = MyClass.RetreiveInfoOfSynbol("MSFT", period, period);
+        //foreach ( var item in income)
+        //{
+        //    Console.WriteLine($"Date:{income.Keys}, Price: {income.Values}");
+        //}
+        //Console.ReadLine();
+    }
+}
--- homeworks/homework-10/sample/TradingApp/TradingApp/TradingDataRetreiver.cs
+++ homeworks/homework-10/sample/TradingApp/TradingApp/TradingDataRetreiver.cs
@@ -13,32 +13,60 @@ namespace TradingApp
     {
         SymbolInfo RetreiveInfo(string symbolName, TimeSpan period, TimeSpan granularity);
     }
-    
+
     public class SymbolInfo : ITradingDataRetreiver
     {
-       
+
         public delegate void MethodContainer(); //Определяем делегат
 
         public event MethodContainer CurrentInfo; // Определяем событие на возвращение данных каждые 2 секунды после вызова метода RetreiveInfo
         public string SymbolName { get; } //Символ компании на бирже
-        public Dictionary<DateTime, decimal> Data { get; }
+        public Dictionary<DateTime, decimal> DataMSFT { get; }
 
+        public Dictionary<DateTime, decimal> DataAAPL { get; }
         public Dictionary<string, Dictionary<DateTime, decimal>> MarketCap { get; } //Словарь с датой и значением
-       
 
+        DateTime dateNow = DateTime.Now;
         Random random = new Random();
-        
 
-        SymbolInfo()
+        public SymbolInfo()
         {
-            MarketCap = new Dictionary<string, decimal>()
+            Dictionary<string, Dictionary<DateTime, decimal>> MarketCap = new Dictionary<string, Dictionary<DateTime, decimal>>();
+            
+            
+            int count = 0;
+            Dictionary<DateTime, decimal> DataMSFT = new Dictionary<DateTime, decimal>();
+            Dictionary<DateTime, decimal> DataAAPL = new Dictionary<DateTime, decimal>();
+
+            var startDate = new DateTime(2023,01,01);
+            var interval = dateNow - startDate; //TimeSpan
+            var interval1 = dateNow - interval; //DateTime
+            for (interval = dateNow - startDate; interval1 <= dateNow; interval1.AddSeconds(2))
             {
-                { "MSFT", Data}, //Может сюда вставить словарь с ключом датой и ценой, нагенерить значений и выдовать по запросу?
-                { "AAPL", },
-                { "AMZN", 300},
-                { "GOOG", 346},
-                { "META", 689}
-            };
+                count++;
+                if (count % 2 == 0)
+                {
+                    DataMSFT.Add(interval1, 308 - (decimal)random.NextDouble());
+                    DataAAPL.Add(interval1, 308 - (decimal)random.NextDouble());
+                }
+                else
+                {
+                    DataMSFT.Add(interval1, 308 + (decimal)random.NextDouble());
+                    DataAAPL.Add(interval1, 308 + (decimal)random.NextDouble());
+                }
+
+            }
+          
+            MarketCap.Add("MSFT", DataMSFT);
+            MarketCap.Add("AAPL", DataAAPL);
+            
+            //{
+            //    //["MSFT"] = new Dictionary<DateTime, decimal>  
+            //    ////{ "AAPL", },
+            //    ////{ "AMZN", 300},
+            //    ////{ "GOOG", 346},
+            //    ////{ "META", 689}
+            //};
             int num = 0;
 
             TimerCallback tm = new TimerCallback(Count);
@@ -48,14 +76,27 @@ namespace TradingApp
         }
         public void Count(object obj)
         {
-            CurrentInfo(); //Возвращение новых данных каждые 2 сек, нужно подписаться на метод RetreiveInfo
+            CurrentInfo += GetCurrentInfo; //Возвращение новых данных каждые 2 сек
         }
         public SymbolInfo RetreiveInfo(string symbolName, TimeSpan period, TimeSpan granularity)
         {
+            SymbolInfo info = new SymbolInfo();
+           
+            var price = MarketCap[symbolName][dateNow - period];
+            return info;
+        }
+        public Dictionary<DateTime, decimal> RetreiveInfoOfSynbol(string symbolName, TimeSpan period, TimeSpan granularity)
+        {
+            SymbolInfo info = new SymbolInfo();
+
             var price = MarketCap[symbolName];
-            return ;
+            return price;
+        }
+        public void GetCurrentInfo()
+        {
+            Console.WriteLine("dw"); // возвращать новые данные
         }
         
-        
     }
+    
 }
