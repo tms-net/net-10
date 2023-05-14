a72c1fc6d03cfa60433f388357e3cc9c1de0bb46
dfcef5 <129785854+dfcef5@users.noreply.github.com>
2023-05-14 22:46:06 +0300

    qqqq

---
+++ TradingApp/TradingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// delegate bool InfoUpdated(SymbolInfo symbolInfo);
+
+// UI - Павел Дунец
+    // Основной символ (тикер) -> ввод пользователя
+        // Текущее значение цены
+        // Информация по символу
+
+    // Сопутствующие символы
+        // Значение
+        // Изменение (↑ | ↓)
+
+    // Возможность изменения частоты InfoUpdated += (Handler)
+    // Функции
+
+
