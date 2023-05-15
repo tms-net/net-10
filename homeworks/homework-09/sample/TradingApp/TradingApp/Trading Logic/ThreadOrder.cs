6cadaa2eab7497b9c8597c5b8fb8847776fe5611
Andrei_Zanka <dfcef.dfcef5\2gmail.com>
2023-05-15 16:39:49 +0300

    threadOrder update

--- homeworks/homework-09/sample/TradingApp/TradingApp/Trading Logic/ThreadOrder.cs
+++ homeworks/homework-09/sample/TradingApp/TradingApp/Trading Logic/ThreadOrder.cs
@@ -3,66 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace TradingApp.Trading_Logic
 {
     internal class ThreadOrder
     {
-        static bool orderstatus; // отслеживание состояния ордера
-        static double requestedPrice; // запрашиваемая цена
-        static double currentPrice; //текущая цена
+        static bool orderStatus; // отслеживание состояния ордера
+        public double requestedPrice { get; set; } // запрашиваемая цена
+        public double currentPrice { get; set; } //текущая цена
 
         static void placeOrder()
         {
-            Console.WriteLine("Ордер выставлен по цене: " + requestedPrice);
-            orderstatus = true;
+            Console.WriteLine("Ордер выставлен");
+            orderStatus = true;
         }
         static void cancelOrder()
         {
-            Console.WriteLine("Ордер снят: ");
-            orderstatus = false;
+            Console.WriteLine("Ордер снят");
+            orderStatus = false;
         }
 
-        static void Threadbody()
+        static void Threadbody(ThreadOrder threadOrder)
         {
-            Thread threadOrder = new Thread(Threadcheck);
-            threadOrder.Start();
+
+            Thread threadBody = new Thread(Threadcheck);
+            threadBody.Start();
             Thread.Sleep(1000);
             placeOrder();
             Thread.Sleep(1000);
-            currentPrice = 800/*ссылаемся на цену с биржи */;
+            ////currentPrice = 800/*ссылаемся на цену с биржи */;
+            threadOrder.currentPrice;
             Thread.Sleep(1000);
-            orderstatus = false;
-            threadOrder.Join();
+            orderStatus = false;
+            threadBody.Join();
 
         }
         /// <summary>
         /// сравниваем текущую цену с ценой биржи
         /// </summary>
-        static void Threadcheck()
+        static void Threadcheck(ThreadOrder threadOrder, OrderType orderType)
         {
             //OrderType MyOrder1 = OrderType.Buy;
-            OrderType MyOrder2 = OrderType.Sell;
-            while (orderstatus)
+            //OrderType MyOrder2 = OrderType.Sell;
+            while (orderStatus)
             {
-                if (MyOrder2 == OrderType.Sell)
+                if (orderType == OrderType.Sell)
                 {
-                    if (currentPrice <= requestedPrice * 0.8d)
+                    if (threadOrder.currentPrice <= threadOrder.requestedPrice * 0.8d)
                     {
                         cancelOrder();
                     }
                 }
                 else
                 {
-                    if (currentPrice >= requestedPrice * 1.5d)
+                    if (threadOrder.currentPrice >= threadOrder.requestedPrice * 1.5d)
                     {
                         cancelOrder();
                     }
                 }
+                return;
 
             }
 
         }
+        static void Main(string[] args)
+        {
+            static int CheckOrderPrice(double currentPrice, double dinamicPrice)
+            {
+                ThreadOrder threadOrder = new ThreadOrder();
+                threadOrder.requestedPrice = dinamicPrice;
+                threadOrder.currentPrice = currentPrice;
+                Threadbody(threadOrder);
 
+            }
+
+        }
     }
 }
+
+
+
