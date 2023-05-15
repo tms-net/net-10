72af28f77722b1bcc11a06542d2d3d07a135793c
Dmitry Andrienko <d.andrienko@live.com>
2023-05-15 20:48:45 +0300

    Add preliminary implementation of UI and Data Retreiver

--- homeworks/homework-10/sample/TradingApp/TradingApp/Program.cs
+++ homeworks/homework-10/sample/TradingApp/TradingApp/Program.cs
@@ -1,18 +1,249 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TradingApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Отображение
+        //  - Экраны (Screen) ; Страница (Page) ; View
+        //      - Home (Главный)
+        //          - Текущие данные (символ, баланс, ...)
+        //      - Опции отображения
+        //      - Создание ордера
+
+        Console.WriteLine("Hello, Trading Platform!");
+
+        // Получить зависимости
+        //  - ITradingDataRetreiver
+        //  - ITradingLogic
+
+        var random = new Random();
+        var tradingDataRetreiver = new TradingDataRetreiver();
+        var tradingLogic = new TradingLogic();
+        var currentBalance = random.Next(1000, 100000);
+
+        // Получить информацию для отображения данных
+        var symbolName = TradingDataFromConsole.GetSymbolFromConsole();
+        var period = TradingDataFromConsole.GetPeriodFromConsole();
+        var granularity = TradingDataFromConsole.GetGranularityFromConsole();
+
+        // Отобразить данные
+        var symbolData = tradingDataRetreiver.RetreiveInfo(symbolName, period, granularity);
+
+        var console = new TradingDataFromConsole(symbolData, currentBalance);
+
+        // Опции для изменения отображения данных
+        console.ShowMainInfo();        
+
+        // Опции для создания ордера
+        Console.WriteLine("Опции:");
+        Console.WriteLine(" 1. Изменить гранулярность");
+        Console.WriteLine(" 2. Изменить период");
+        Console.WriteLine(" 3. Создать ордер");
+
+        while (true)
+        {
+            var option = TradingDataFromConsole.GetOptionFromConsole();
+
+            // Обработка опции
+
+            console.ShowMainInfo();
+        }
+    }
+
+    internal class TradingDataFromConsole
+    {
+        private SymbolInfo _symbolData;
+        private int _currentBalance;
+
+        public TradingDataFromConsole(SymbolInfo symbolData, int currentBalance)
+        {
+            _symbolData = symbolData;
+            _currentBalance = currentBalance;
+        }
+
+        internal static string GetOptionFromConsole()
+        {
+            string? result;
+
+            while (true)
+            {
+                Console.WriteLine("Input Symbol name:");
+                result = Console.ReadLine();
+                if (result == null || result.Length != 0 || result.Length > 2)
+                {
+                    Console.WriteLine($"Incorrect Option name {result}");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        internal static string GetSymbolFromConsole()
+        {
+            string? result;
+
+            while (true)
+            {
+                Console.WriteLine("Input Symbol name:");
+                result = Console.ReadLine();
+                if (result == null || result.Length != 0)
+                {
+                    Console.WriteLine($"Incorrect Symbol name {result}");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+        internal static TimeSpan GetPeriodFromConsole()
+        {
+            DateTime dateTimeFrom = DateTime.Now;
+            DateTime dateTimeTo = DateTime.Now;
+            Console.WriteLine("For retrieving info need period");
+            while (true)
+            {
+                Console.WriteLine("Input date FROM: ");
+                if (!DateTime.TryParse(Console.ReadLine(), out dateTimeFrom))
+                {
+                    Console.WriteLine($"Uncorrect date {dateTimeFrom}");
+                }
+                else { break; }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Input date TO: ");
+                if (!DateTime.TryParse(Console.ReadLine(), out dateTimeTo) || dateTimeTo < dateTimeFrom)
+                {
+                    Console.WriteLine($"Uncorrect date {dateTimeTo}");
+                }
+                else
+                {
+                    return dateTimeTo - dateTimeFrom;
+                }
+            }
+        }
+        internal static TimeSpan GetGranularityFromConsole()
+        {
+            while (true)
+            {
+                Console.WriteLine("For retrieving info choose granularity (1 - Day, 2 - Week, 3 - Month, 4 - Year)");
+                switch (Console.ReadLine())
+                {
+                    case "1": return DateTime.Now.AddDays(1) - DateTime.Now;
+                    case "2": return DateTime.Now.AddDays(7) - DateTime.Now;
+                    case "3": return DateTime.Now.AddMonths(1) - DateTime.Now;
+                    case "4": return DateTime.Now.AddYears(1) - DateTime.Now;
+                    default: Console.WriteLine("Unavailable granularity code"); break;
+                }
+            }
+        }
+
+
+        internal static decimal SetDealPriceFromConsole()
+        {
+            decimal result;
+
+            while (true)
+            {
+                Console.WriteLine("Input Deal price:");
+
+                if (!decimal.TryParse(Console.ReadLine(), out result))
+                {
+                    Console.WriteLine($"Can't convert inputed text into decimal: {result}");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        internal static DealStatus SetStatusFromConsole()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Input Deal status (1 - {DealStatus.Pending}, 2 - {DealStatus.Completed}, 3 - {DealStatus.Cancelled}):");
+                switch (Console.ReadLine())
+                {
+                    case "1": return DealStatus.Pending;
+                    case "2": return DealStatus.Completed;
+                    case "3": return DealStatus.Cancelled;
+                    default: Console.WriteLine("Unavailable status code"); break;
+                }
+            }
+        }
+
+        internal static int SetQuantityFromConsole()
+        {
+            int result = 0;
+            while (true)
+            {
+                Console.WriteLine("Input Quantity: ");
+                if (!int.TryParse(Console.ReadLine(), out result))
+                {
+                    Console.WriteLine($"Can't convert inputed text into int: {result}");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        internal static OrderPriceType SetOrderPriceTypeFromConsole()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Input Order price type (1 - {OrderPriceType.Market}, 2 - {OrderPriceType.Price}):");
+                switch (Console.ReadLine())
+                {
+                    case "1": return OrderPriceType.Market;
+                    case "2": return OrderPriceType.Price;
+                    default: Console.WriteLine("Unavailable Order price type code"); break;
+                }
+            }
+        }
+
+        internal static OrderType SetOrderTypeTypeFromConsole()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Input Order type (1 - {OrderType.Buy}, 2 - {OrderType.Sell}):");
+                switch (Console.ReadLine())
+                {
+                    case "1": return OrderType.Buy;
+                    case "2": return OrderType.Sell;
+                    default: Console.WriteLine("Unavailable Order type code"); break;
+                }
+            }
+        }
+
+        internal void ShowMainInfo()
+        {
+            Console.Clear();
+            Console.WriteLine($" Данные {_symbolData.SymbolName}:");
+            Console.WriteLine($" Текущая цена: {_symbolData.Data.Last().Value}");
+            Console.WriteLine($" Капитализация: {_symbolData.MarketCap}");
+            Console.WriteLine($" Баланс: {_currentBalance}");
+        }
+    }
+}
 
 // delegate bool InfoUpdated(SymbolInfo symbolInfo);
 
 // UI - Павел Дунец
-    // Основной символ (тикер) -> ввод пользователя
-        // Текущее значение цены
-        // Информация по символу
+// Основной символ (тикер) -> ввод пользователя
+// Текущее значение цены
+// Информация по символу
 
-    // Сопутствующие символы
-        // Значение
-        // Изменение (↑ | ↓)
+// Сопутствующие символы
+// Значение
+// Изменение (↑ | ↓)
 
-    // Возможность изменения частоты InfoUpdated += (Handler)
-    // Функции
-
-
+// Возможность изменения частоты InfoUpdated += (Handler)
+// Функции
--- homeworks/homework-10/sample/TradingApp/TradingApp/TradingDataRetreiver.cs
+++ homeworks/homework-10/sample/TradingApp/TradingApp/TradingDataRetreiver.cs
@@ -1,34 +1,108 @@
-﻿namespace TradingApp
-{
-    // Получение - Евгений Липай
-        // Получить данные по тикеру и частоте
-        // InfoUpdated()
-        // Получение данных для похожих символов
+﻿using System;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-    // Получение
+namespace TradingApp
+{
+    //   - Получить данные по тикеру и частоте
+    //      - InfoUpdated()
+    //      - Использовать делегат для уведомления компонент пользовательского интерфейса о наличии новых цен на акции.
+    //      - Класс должен получать новые данные каждые X секунд и уведомлять компонент пользовательского интерфейса.
+    //   - Получение данных для похожих символов
+
     public interface ITradingDataRetreiver
     {
-
-        // От 01.01.2023 до now
-        // TimeSpan (from -> to) - год, день, неделя
-        // Enum -> год, день, неделя (Period)
-
-        // гранулярность
-        // Enum
-        // Timespan
         SymbolInfo RetreiveInfo(string symbolName, TimeSpan period, TimeSpan granularity);
     }
 
     public class SymbolInfo
     {
-        public string SymbolName { get; }
-        public string MarketCap { get; }
+        public string SymbolName { get; } //Символ компании на бирже
+        public decimal MarketCap { get; } //Словарь с датой и значением
         public IDictionary<DateTime, decimal> Data { get; }
-
         public event Action<SymbolInfo> SymbolUpdated;
+
+        public SymbolInfo()
+        {
+            // Сгененрировать объем акций
+        }
     }
 
-    internal class TradingDataRetreiver
+    internal class TradingDataRetreiver : ITradingDataRetreiver
     {
+        const int MinimalGranularityMs = 5 * 60 * 1000;
+
+        private readonly Random _random = new Random();
+        private readonly Dictionary<DateTime, decimal> _dataMSFT = new Dictionary<DateTime, decimal>();
+        private readonly Dictionary<DateTime, decimal> _dataAAPL = new Dictionary<DateTime, decimal>();
+
+        public TradingDataRetreiver()
+        {
+            GenerateData();
+
+            // Дополнение текущими данными
+            //  - Каждые 5 мин
+            
+            Timer timer = new Timer(UpdateData, null, MinimalGranularityMs, MinimalGranularityMs);
+        }
+
+        public SymbolInfo RetreiveInfo(string symbolName, TimeSpan period, TimeSpan granularity)
+        {
+            // Получение данных
+
+            // Аггрегация данных
+
+            throw new NotImplementedException();
+        }
+
+        private void UpdateData(object? state)
+        {
+            var dateNow = DateTime.UtcNow;
+
+            var rnd = _random.Next(1);
+            var msftStartPrice = _dataMSFT.Last().Value; // IEnumerable.Last()
+            var aaplStartPrice = _dataAAPL.Last().Value;
+
+            if (rnd == 0)
+            {
+                _dataMSFT.Add(dateNow, msftStartPrice - msftStartPrice * (decimal)_random.NextDouble());
+                _dataAAPL.Add(dateNow, aaplStartPrice - aaplStartPrice * (decimal)_random.NextDouble());
+            }
+            else
+            {
+                _dataMSFT.Add(dateNow, msftStartPrice + msftStartPrice * (decimal)_random.NextDouble());
+                _dataAAPL.Add(dateNow, aaplStartPrice + aaplStartPrice * (decimal)_random.NextDouble());
+            }
+        }
+
+        private void GenerateData()
+        {
+            // Генерация исторических данных
+            //   - MSFT и AAPL
+            //   - За день
+            //   - Гранулярность -> Enum: 5 min, 30 min, 1h
+
+            int count = 0;
+            const int msftStartPrice = 308;
+            const int aaplStartPrice = 200;
+
+            var dateNow = DateTime.UtcNow;
+            var startDate = dateNow.AddDays(-1);
+
+            for (var date = startDate; date <= dateNow; date = date.AddMilliseconds(MinimalGranularityMs))
+            {
+                count++;
+                if (count % 2 == 0)
+                {
+                    _dataMSFT.Add(date, msftStartPrice - msftStartPrice * (decimal)_random.NextDouble());
+                    _dataAAPL.Add(date, aaplStartPrice - aaplStartPrice * (decimal)_random.NextDouble());
+                }
+                else
+                {
+                    _dataMSFT.Add(date, msftStartPrice + msftStartPrice * (decimal)_random.NextDouble());
+                    _dataAAPL.Add(date, aaplStartPrice + aaplStartPrice * (decimal)_random.NextDouble());
+                }
+            }
+        }
     }
 }
