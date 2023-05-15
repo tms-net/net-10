0d740dee07990af9094b59a426a1369f47f8a42d
Nikita <mr.nikitakachura@gmail.com>
2023-05-15 16:12:31 +0200

    Added solution from ITradingLogic team

---
+++ homeworks/homework-10/sample/TradingApp/TradingApp/Trading Logic/OrderInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TradingApp
+{
+    public class OrderInfo
+    {
+        public string Symbol { get; set; }
+        public decimal DealPrice { get; set; }
+        public OrderType OrderType { get; set; }
+        public DealStatus Status { get; set; }
+    }
+}
+
