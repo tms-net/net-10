8263382cb4581ef636c35aa0450b2236b3022fae
Dzmitry-Patapau <potapovdimitry@gmail.com>
2023-05-16 15:16:18 +0300

    HW9

---
+++ homeworks/homework-09/results/patapau/ATM/ATMClient.cs
@@ -0,0 +1,75 @@
+﻿namespace Banking
+{
+    internal class ATMClient
+    {
+        private ATM _atm;
+
+        private decimal? _accountBalance; // Nullable Value Type
+
+        public event EventHandler<ClientBalanceEventArgs> _clientBalance;
+
+        public ATMClient(ATM atm)
+        {
+            _atm = atm;
+        }
+
+        /// <summary>
+        /// Вставить карточку
+        /// </summary>
+        /// <param name="accountBalance">баланс карточки</param>
+        public void InsertCard(decimal accountBalance)
+        {
+            _accountBalance = accountBalance;
+        }
+
+        /// <summary>
+        /// Снять средства из банкомата
+        /// </summary>
+        /// <param name="amount">сумма для снятия</param>
+        public void Withdraw(long amount)
+        {
+            if (amount <= _accountBalance)
+            {
+                _atm.WithdrawMoney(amount);
+                _accountBalance -= amount;
+            }
+        }
+
+        /// <summary>
+        /// .Пополнить банкомат средствами
+        /// </summary>
+        /// <param name="amount">сумма пополнения</param>
+        public void TopUp(long amount)
+        {
+            if (amount > 0)
+            {
+                _atm.TopUpMoney(amount);
+                _accountBalance += amount;
+            }
+        }
+
+        /// <summary>
+        /// Просмотр баланса пользователем
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void ViewBalance()
+        {
+            ClientBalanceEventArgs args = new ClientBalanceEventArgs(_accountBalance);
+            if (_clientBalance != null)
+            {
+                _clientBalance(this, args);
+            }
+        }
+    }
+
+
+    public class ClientBalanceEventArgs
+    {
+        public decimal? Balance { get; set; }
+
+        public ClientBalanceEventArgs(decimal? balance)
+        {
+            Balance = balance;
+        }
+    }
+}
---
+++ homeworks/homework-09/results/patapau/ATM/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Banking;
+
+var random = new Random();
+var atm = new ATM(10000);
+
+
+atm._transactionCompleted += (sender, args) =>
+{
+    Console.WriteLine($"Transaction completed: {args.BalanceAfter}");
+};
+
+for (int i = 0; i < 10; i++)
+{
+    var client = new ATMClient(atm);
+    client._clientBalance += GetClientBalance;
+    Console.Write($"Client {i + 1} ");
+    DoRandomActions(client);
+}
+
+void GetClientBalance(object sender, ClientBalanceEventArgs args)
+{
+    Console.WriteLine($"Client balance {args.Balance}");
+}
+
+void DoRandomActions(ATMClient client)
+{
+    var actions = new[] { client.Withdraw, client.TopUp };
+
+    client.InsertCard(random.Next(100, 10000));
+    client.ViewBalance();
+    var numberOfActions = random.Next(10);
+    for (int i = 0; i < numberOfActions; i++)
+    {
+        actions[random.Next(0,2)](random.Next(10, 1000));
+        client.ViewBalance();
+    }
+}
