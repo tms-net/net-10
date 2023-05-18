a45ca1d54f49545714412feb8829cc5dd05e0844
Dmitry Andrienko <d.andrienko@live.com>
2023-05-18 17:20:09 +0300

    Add Files to Trading App

---
+++ homeworks/homework-10/sample/TradingApp/TradingApp/Trading Data/TradingData.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace TradingApp.Data
+{
+    public class TradingData
+    {
+        private string _password = "password";
+
+        // публичный конструктор по умолчанию
+        public TradingData()
+        {
+        }
+
+        // правильные модификаторы доступа
+        public string Version { get; set; }
+
+        [JsonPropertyName("metadata")]
+        public TradingMedatata MetaData { get; set; }
+
+        [JsonPropertyName("data")]
+        [XmlElement(ElementName = "data")]
+        public PriceData Data { get; set; }
+
+        // циклические ссылки
+        public TradingData OldData { get; set; }
+
+        public TradingData NewData { get; set; }
+    }
+
+    public class TradingMedatata
+    {
+    }
+
+    public class PriceData
+    {
+    }
+}
