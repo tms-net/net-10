48a3fb3726a3e036fbfa9fb7a86e222bc4da3296
PavelNTD <129785836+PavelNTD@users.noreply.github.com>
2023-05-20 14:49:48 +0300

    Homework 5. Basic Task (#36)

---
+++ homeworks/homework-05/results/dunec/ConsoleAppWithOOP/ConsoleAppWithOOP/Vehicles/Motorcycle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithOOP.Vehicles
+{
+    internal class Motorcycle : Vehicle, IHaveEngine
+    {
+        private bool _tripodEjected = true;
+
+        public Motorcycle(string make, string model, int year, double weigh) : base(make, model, year, weigh)
+        {
+        }
+
+        public bool SuccesStart()
+        {
+            Random ran = new Random();
+            if (ran.Next(1, 20) > 5)
+            { return true; }
+            else
+            { return false; }
+        }
+        /*
+        public bool SuccesStop()
+        {
+            Random ran = new Random();
+            if (ran.Next(1, 5) > 2)
+            { return true; }
+            else
+            { return false; }
+        } //*/
+
+        override public void Start()
+        {
+            CurrentSpeed = 88;
+            _tripodEjected = false;
+        }
+
+        override public void Stop()
+        {
+            base.Stop();
+            _tripodEjected = true;
+        }
+    }
+}
