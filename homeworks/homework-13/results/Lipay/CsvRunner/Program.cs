b6767f24585cfd62b13e59bac8f5ed7f89a81917
Женік Липай <lipay51131@mail.ru>
2023-05-23 17:19:19 +0300

    Init project HW-13

---
+++ homeworks/homework-13/results/Lipay/CsvRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main()
+    {
+        // 1. Создать набор данных
+
+        // 2. Преобразовать в .csv формат
+
+        // 3. Вывести результат / сохранить в файл
+
+    }
+}
