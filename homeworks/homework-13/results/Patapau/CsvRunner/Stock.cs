1af687efc136896ab49b26999559e85252fad677
Dzmitry-Patapau <potapovdimitry@gmail.com>
2023-05-24 17:08:41 +0300

    HW13 Serializer

---
+++ homeworks/homework-13/results/Patapau/CsvRunner/Stock.cs
@@ -0,0 +1,23 @@
+﻿using CsvSerializer;
+
+namespace CsvRunner
+{
+    [Serializable]
+    public class Stock : BaseStock
+    {
+        private readonly string _name;
+
+        static Stock Empty => new Stock();
+
+        [CsvHeader("Ticker")]
+        public string Ticker { get; set; }
+        [CsvHeader("Price")]
+        public decimal CurrentPrice { get; set; }
+    }
+
+    public class BaseStock
+    {
+        [CsvHeader("Capitalization")]
+        public decimal Capitalization { get; set; }
+    }
+}
