86bcdeafaebb974086f5e7a6397355466f8342e1
Dmitry Andrienko <d.andrienko@live.com>
2023-05-25 11:32:00 +0300

    Add hw 14

--- homeworks/homework-13/sample/CsvRunner/Stock.cs
+++ homeworks/homework-13/sample/CsvRunner/Stock.cs
@@ -13,10 +13,27 @@ namespace CsvRunner
         public string Ticker { get; set; }
         
         public decimal CurrentPrice { get; set; }
+
+        public BankReport Report { get; set; }
+
+        public decimal[] Prices { get; set; }
     }
 
     public class BaseStock
     {
         public decimal Capitalization { get; set; }
+    }
+    
+    public class BankReport
+    {
+        public Rating Rating { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public enum Rating
+    {
+        Wait,
+        Buy,
+        Sell
     }
 }
