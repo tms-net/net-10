b22082ed338600188fb65c34cddac36219e32851
nastya <nastiusha_tom@icloud.com>
2023-05-27 21:01:58 +0300

    HW-13

---
+++ homeworks/homework-13/results/tomchuk/HW-13/CsvSerializer/CsvSerializer.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+
+namespace CsvSerializer
+{
+    public class CsvSerializer
+    {
+        // char == byte
+        // ['H','e','l','l']
+        public static string Serialize<T>(IEnumerable<T> lines)
+        {
+            var type = typeof(T);
+
+            var properties = type.GetProperties();
+
+            foreach (var property in properties)
+            {
+                var attr = property.GetCustomAttribute<CsvHeaderAttribute>();
+            }
+
+            // 1. Заголовок
+            //   - Определить колонки
+            //      - Свойства класса
+            //      - .......
+
+            
+
+            // 2. Строки
+            //   - Получить значения нужных свойств
+            //      - форматирование
+
+            return string.Empty;
+        }
+
+        public static IEnumerable<T> Deserialize<T>(string csv)
+        {
+            // 1. Заголовок
+            //   - Определить колонки
+
+            // 2. Строки
+            //   - Создать объекты
+            //   - Получить значение колонки
+            //   - Установить значения нужных свойств
+
+            return Enumerable.Empty<T>();
+        }
+    }
+}
