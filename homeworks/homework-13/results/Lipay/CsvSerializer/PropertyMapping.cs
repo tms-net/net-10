2d09ebae18eeb8d92ac789da49bc929f71f28af1
Женік Липай <lipay51131@mail.ru>
2023-05-31 16:56:51 +0300

    add new code

---
+++ homeworks/homework-13/results/Lipay/CsvSerializer/PropertyMapping.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace CsvSerializer
+{
+    internal class PropertyMapping
+        {
+            public PropertyInfo PropertyInfo { get; set; }
+            public PropertyMapping ParentProperty { get; internal set; }
+            public Action<object, object> SetProperty { get; internal set; }
+        }
+    
+}
