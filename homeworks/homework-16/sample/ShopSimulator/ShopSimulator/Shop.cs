e92c1e8a6a3ba83d47cc2de4b2eb8f4eadd9f44b
Dmitry Andrienko <d.andrienko@live.com>
2023-06-01 12:04:36 +0300

    Add hw 16

---
+++ homeworks/homework-16/sample/ShopSimulator/ShopSimulator/Shop.cs
@@ -0,0 +1,44 @@
+﻿namespace ShopSimulator
+{
+    public class Shop
+    {
+        private Thread[] _cahierThreads;
+        private Queue<Person> _peopleQueue; // Эмуляция очереди клиентов
+
+        public Shop(int cahierCount)
+        {
+            _peopleQueue = new Queue<Person>();
+            _cahierThreads = new Thread[cahierCount];
+
+            for (int i = 0; i < cahierCount; i++)
+            {
+                _cahierThreads[i] = new Thread(ServeCustomer);
+            }
+        }
+
+        public void Open()
+        {
+            Array.ForEach(_cahierThreads, thread => thread.Start());
+
+            Console.WriteLine($"Добро пожаловать, вас обслуживает {_cahierThreads.Length} касс(ы)");
+        }
+
+        public void Enter(Person person)
+        {
+            // TODO: Реализовать логику постановки клиента в очередь
+
+            Console.WriteLine($"{person.Name} вошел в магазин");
+        }
+
+        public void Close()
+        {
+            // TODO: Реализовать гарантированное обслуживание всех клиентов после закрытия
+        }
+
+        private void ServeCustomer()
+        {
+            // TODO: Реализовать логику обслуживания клиента из очереди
+            // Использовать свойство клиента для эмуляции времени обслуживания с помощью Thread.Sleep()
+        }
+    }
+}
