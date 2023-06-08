c1f59a3425198339b7eb7ea4022f9cc9769b75a9
Nikita <mr.nikitakachura@gmail.com>
2023-06-08 19:13:55 +0200

    Soultion with TreadPool + Semaphore

--- homeworks/homework-17/results/kachura/ShopSimulator/ShopSimulator/ShopWithThreadPool.cs
+++ homeworks/homework-17/results/kachura/ShopSimulator/ShopSimulator/ShopWithThreadPool.cs
@@ -4,33 +4,33 @@ namespace ShopSimulator
 {
     public class ShopWithThreadPool
     {
+        private static Semaphore _semaphore;
         private Queue<Person> _peopleQueue; // Эмуляция очереди клиентов
         private bool _isShopOpen;
-        private object locker = new();        
         private int _cashierCount;
         private readonly int WAITINGTIME = 100;
 
         public ShopWithThreadPool(int cashierCount)
         {
+            _semaphore = new Semaphore(cashierCount, cashierCount);
             _peopleQueue = new Queue<Person>();
             _cashierCount = cashierCount;
         }
 
         public void Open()
         {
-            _isShopOpen = true;
-            for (int i = 0; i < _cashierCount; i++)
-            {
-                ThreadPool.QueueUserWorkItem(RunServing);
-            }
-
+            _isShopOpen = true;            
             Console.WriteLine($"Добро пожаловать");
         }
 
         public void Enter(Person person)
         {
-            Console.WriteLine($"{person.Name} вошел в магазин");
-            _peopleQueue.Enqueue(person);
+            if(_isShopOpen)
+            {
+                Console.WriteLine($"{person.Name} вошел в магазин");
+                _peopleQueue.Enqueue(person);
+                ThreadPool.QueueUserWorkItem(RunServing);
+            }
         }
 
         public void Close()
@@ -46,30 +46,24 @@ namespace ShopSimulator
 
         private void ServeCustomer()
         {
-            while (_isShopOpen || _peopleQueue.Count() > 0)
+            _semaphore.WaitOne();
+
+            Person curPerson = null;
+            curPerson = _peopleQueue.Dequeue();
+
+            if (curPerson != null)
             {
-                Person curPerson = null;
+                Console.WriteLine($"{curPerson.Name} обслуживается сейчас");
+                Thread.Sleep(curPerson.ProcessingTime);
+                Console.WriteLine($"{curPerson.Name} обслужен");
+            }
+            else
+            {
+                Console.WriteLine("Queue is empty. Waiting...");
+                Thread.Sleep(WAITINGTIME);
+            }
 
-                lock (locker)
-                {
-                    if (_peopleQueue.Count() > 0)
-                    {
-                        curPerson = _peopleQueue.Dequeue();
-                    }
-                }
-
-                if (curPerson != null)
-                {
-                    Console.WriteLine($"{curPerson.Name} обслуживается сейчас");
-                    Thread.Sleep(curPerson.ProcessingTime);
-                    Console.WriteLine($"{curPerson.Name} обслужен");
-                }
-                else
-                {
-                    Console.WriteLine("Queue is empty. Waiting...");
-                    Thread.Sleep(WAITINGTIME);
-                }
-            }           
+            _semaphore.Release();
         }
     }
 }
