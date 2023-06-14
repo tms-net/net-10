599fc9183520a8c0ab1cf18152a82b5b395d98c6
MenterCOX <danya.yagnish@mail.ru>
2023-06-14 19:47:23 +0300

    update

---
+++ homeworks/homework-13/results/yagnish/CsvRunner/Stock.cs
@@ -0,0 +1,39 @@
+﻿using CsvSerializer;
+
+namespace CsvRunner
+{
+    [Serializable]
+    public class Stock : BaseStock
+    {
+        private readonly string _name;
+
+        static Stock Empty => new Stock();
+
+        [CsvHeader("символ")]
+        public string Ticker { get; set; }
+        
+        public decimal CurrentPrice { get; set; }
+
+        public BankReport Report { get; set; }
+
+        public decimal[] Prices { get; set; }
+    }
+
+    public class BaseStock
+    {
+        public decimal Capitalization { get; set; }
+    }
+    
+    public class BankReport
+    {
+        public Rating Rating { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public enum Rating
+    {
+        Wait,
+        Buy,
+        Sell
+    }
+}
