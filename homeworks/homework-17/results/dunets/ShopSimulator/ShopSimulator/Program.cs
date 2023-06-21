fb7317410b257de76d3bce7d01ca0b547fcf9011
PavelNTD <dunecpasha@yandex.ru>
2023-06-21 11:18:25 +0300

    Sample

---
+++ homeworks/homework-17/results/dunets/ShopSimulator/ShopSimulator/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ShopSimulator;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Hello, Shop!");
+
+var random = new Random();
+var shop = new Shop(3);
+
+shop.Open();
+
+for (int i = 0; i < 100; i++)
+{
+    var person = new Person(random.Next(100, 1000), i + 1);
+    shop.Enter(person);
+
+    Thread.Sleep(100);
+}
+
+shop.Close();
