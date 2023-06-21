8be3421088d7e0b0c6369bd3449f4eaf60e63223
PavelNTD <dunecpasha@yandex.ru>
2023-06-21 12:04:06 +0300

    Homework 16. Finished (him)

--- homeworks/homework-16/results/dunec/ShopSimulator/ShopSimulator/Program.cs
+++ homeworks/homework-16/results/dunec/ShopSimulator/ShopSimulator/Program.cs
@@ -1,19 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ShopSimulator;
 
-Console.WriteLine("Hello, Shop!");
-
-var random = new Random();
-var shop = new Shop(3);
-
-shop.Open();
-
-for (int i = 0; i < 100; i++)
+internal class Program
 {
-    var person = new Person(random.Next(100, 1000), i + 1);
-    shop.Enter(person);
+    private static void Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-    Thread.Sleep(100);
+        Console.WriteLine("Hello, Shop!");
+
+        var random = new Random();
+        var shop = new Shop(30);
+
+        shop.Open();
+
+        for (int i = 0; i < 100; i++)
+        {
+            var person = new Person(random.Next(100, 1000), i + 1);
+            shop.Enter(person);
+
+            Thread.Sleep(random.Next(100));
+        }
+
+        shop.Close();
+    }
 }
-
-shop.Close();
--- homeworks/homework-16/results/dunec/ShopSimulator/ShopSimulator/Shop.cs
+++ homeworks/homework-16/results/dunec/ShopSimulator/ShopSimulator/Shop.cs
@@ -1,13 +1,16 @@
-﻿namespace ShopSimulator
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ShopSimulator
 {
     public class Shop
     {
         private Thread[] _cahierThreads;
-        private Queue<Person> _peopleQueue; // Эмуляция очереди клиентов
-
+        private ConcurrentQueue<Person> _peopleQueue; // Эмуляция очереди клиентов
+        private bool _isOpen;
         public Shop(int cahierCount)
         {
-            _peopleQueue = new Queue<Person>();
+            _peopleQueue = new ConcurrentQueue<Person>();
             _cahierThreads = new Thread[cahierCount];
 
             for (int i = 0; i < cahierCount; i++)
@@ -18,6 +21,7 @@
 
         public void Open()
         {
+            _isOpen = true;
             Array.ForEach(_cahierThreads, thread => thread.Start());
 
             Console.WriteLine($"Добро пожаловать, вас обслуживает {_cahierThreads.Length} касс(ы)");
@@ -26,17 +30,48 @@
         public void Enter(Person person)
         {
             // TODO: Реализовать логику постановки клиента в очередь
-
-            Console.WriteLine($"{person.Name} вошел в магазин");
+            if (_isOpen)
+            {
+                lock (_peopleQueue)
+                {
+                    _peopleQueue.Enqueue(person);                    
+                }
+                Console.WriteLine($"{person.Name} вошел в магазин");
+            }            
         }
 
         public void Close()
         {
+            _isOpen = false;
+            Console.WriteLine("Store is closing. There's no escape! Surrender!");
+            while (_peopleQueue.Count > 0 ) 
+            {
+                Thread.Sleep(100);
+            }
             // TODO: Реализовать гарантированное обслуживание всех клиентов после закрытия
         }
 
         private void ServeCustomer()
         {
+            while (_isOpen || _peopleQueue.Count > 0)
+            {
+                if (_peopleQueue.Count == 0)
+                {
+                    Thread.Sleep(new Random().Next(100));
+                }
+                else
+                {
+                    lock (_peopleQueue)
+                    {
+                        if (_peopleQueue.TryDequeue(out var person))
+                        {
+                            Thread.Sleep(person.ProcessingTime);
+                            Console.WriteLine($"{person.Name} served with a few grams of lead");
+                        }
+                    }
+                }                    
+                
+            }
             // TODO: Реализовать логику обслуживания клиента из очереди
             // Использовать свойство клиента для эмуляции времени обслуживания с помощью Thread.Sleep()
         }
