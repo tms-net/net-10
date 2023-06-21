fb5b57c9c5318f410e35e490ba4e2f410d5f39a7
PavelNTD <dunecpasha@yandex.ru>
2023-06-21 12:58:23 +0300

    Homework-17. Fin

--- homeworks/homework-17/results/dunets/ShopSimulator/ShopSimulator/Program.cs
+++ homeworks/homework-17/results/dunets/ShopSimulator/ShopSimulator/Program.cs
@@ -1,22 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ShopSimulator;
+using System;
+using System.Threading;
 
-Console.InputEncoding = System.Text.Encoding.UTF8;
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-Console.WriteLine("Hello, Shop!");
-
-var random = new Random();
-var shop = new Shop(3);
-
-shop.Open();
-
-for (int i = 0; i < 100; i++)
+internal class Program
 {
-    var person = new Person(random.Next(100, 1000), i + 1);
-    shop.Enter(person);
+    private static void Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-    Thread.Sleep(100);
+        Console.WriteLine("Hello, Shop!");
+
+
+        var random = new Random();
+        //var shop = new Shop(3);
+
+        var shop = new ShopWithThreadPool(13);
+
+        shop.Open();
+
+        for (int i = 0; i < 100; i++)
+        {
+            var person = new Person(random.Next(100, 1000), i + 1);
+            shop.Enter(person);
+
+            Thread.Sleep(random.Next(100));
+        }
+
+        shop.Close();
+
+        Console.ReadLine();
+    }
+
 }
-
-shop.Close();
--- homeworks/homework-17/results/dunets/ShopSimulator/ShopSimulator/ShopWithThreadPool.cs
+++ homeworks/homework-17/results/dunets/ShopSimulator/ShopSimulator/ShopWithThreadPool.cs
@@ -1,6 +1,49 @@
-﻿namespace ShopSimulator
+﻿using System.Collections.Concurrent;
+
+namespace ShopSimulator
 {
     internal class ShopWithThreadPool
     {
+        private Queue<Person> _peopleQueue; // Эмуляция очереди клиентов
+        private bool _isOpen;
+
+        public ShopWithThreadPool(int cahierCount)
+        {
+            _peopleQueue = new Queue<Person>();
+            
+            ThreadPool.SetMaxThreads(cahierCount, 1);
+            //ThreadPool.QueueUserWorkItem(ServeCustomer);
+        }
+
+        public void Open()
+        {
+            _isOpen = true;
+            
+            ThreadPool.GetAvailableThreads(out var workerThreads, out _);
+            Console.WriteLine($"Добро пожаловать, вас обслуживает {workerThreads} касс(ы)");
+        }
+
+        public void Enter(Person person)
+        {
+            if (_isOpen)
+            {
+                _peopleQueue.Enqueue(person);
+                Console.WriteLine($"{person.Name} вошел в магазин");
+                ThreadPool.QueueUserWorkItem(ServeCustomer, _peopleQueue.Dequeue());
+            }
+        }
+
+        public void Close()
+        {
+            _isOpen = false;
+            Console.WriteLine("Store is closing. There's no escape! Surrender!");
+        }
+
+        public void ServeCustomer(object persona)
+        {
+            var person = (Person) persona;
+            Thread.Sleep(person.ProcessingTime);
+            Console.WriteLine($"{person.Name} served with a few grams of lead");
+        }
     }
 }
