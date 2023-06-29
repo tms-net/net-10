21622cb429e5bd36bb0edd17b62a8b12d458d997
PavelNTD <dunecpasha@yandex.ru>
2023-06-29 23:38:13 +0300

    Homework 19. Finished

--- homeworks/homework-19/results/dunec/AsyncIO/AsyncIO/Program.cs
+++ homeworks/homework-19/results/dunec/AsyncIO/AsyncIO/Program.cs
@@ -1,20 +1,44 @@
-﻿class Program
+﻿using System.Net.Sockets;
+
+class Program
 {
+    static readonly object locker = new object();
+    static int count;
+    static List<string> threadsList = new List<string>();
     private static async Task Main()
     {
+        
         var content = CreateFileContent();
         var tasks = new List<Task>(100);
+        var threadsList = new List<string>();
 
         for (int i = 0; i < 10; i++)
         {
-            tasks.Add(WriteFile($"file-{i}.txt", content));
+            tasks.Add(WriteFile($"file-{i}.txt", content, threadsList));
         }
 
         await Task.WhenAll(tasks);
+        
     }
 
-    private static async Task WriteFile(string path, string content)
+    private static async Task WriteFile(string path, string content, List<string> threadsList)
     {
+       // lock(locker)
+       // {
+            AddNewThreadId(Thread.CurrentThread.ManagedThreadId, ref threadsList);
+       // }
+        
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(Directory.GetCurrentDirectory(), path)))
+        {
+            await outputFile.WriteAsync(content);
+        }
+
+        //lock (locker)
+        //{
+            AddNewThreadId(Thread.CurrentThread.ManagedThreadId, ref threadsList);
+        //}
+            
+        Console.WriteLine($"At moment of writing {path} has used {threadsList.Count} threads ({String.Join(",", threadsList)})");
         // TODO: Записать данные в файл
     }
 
@@ -34,4 +58,15 @@
 
         return System.Text.Encoding.UTF8.GetString(buffer);
     }
+
+    private static void AddNewThreadId(int id, ref List<string> threadsList)
+    {
+        if (threadsList.IndexOf(id.ToString()) == -1)
+        {
+            threadsList.Add(id.ToString());
+        }
+    }
+
+    
+
 }
