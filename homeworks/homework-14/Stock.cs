b1ef00ff28d939864df0b9d8c1d7db5fe6dcdf25
gogurets <129603830+gogurets@users.noreply.github.com>
2023-07-03 17:45:25 +0300

    Zhdanov hw 14

---
+++ homeworks/homework-14/Stock.cs
@@ -0,0 +1,13 @@
+﻿using CsvSerializer;
+using CcvSerializer;
+
+namespace CsvSerializer.Example
+{
+    [CsvHeader("Symbol,Price,Volume")]
+    public class Stock
+    {
+        public string? Symbol { get; set; }
+        public decimal Price { get; set; }
+        public int Volume { get; set; }
+    }
+}
