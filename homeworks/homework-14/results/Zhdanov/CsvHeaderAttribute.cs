7b30b7350f223308a3641e5ee8818bbb58251e3c
gogurets <129603830+gogurets@users.noreply.github.com>
2023-07-04 20:52:20 +0300

    Add files via upload

---
+++ homeworks/homework-14/results/Zhdanov/CsvHeaderAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace CcvSerializer
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CsvHeaderAttribute : Attribute
+    {
+        public string Header { get; }
+
+        public CsvHeaderAttribute(string header)
+        {
+            Header = header;
+        }
+    }
+}
