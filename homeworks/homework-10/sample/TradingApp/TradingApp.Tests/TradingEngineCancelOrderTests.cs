4229310cd637a537a72a5f859a9423da0b3aa7e1
Nikita <mr.nikitakachura@gmail.com>
2023-07-18 17:19:20 +0200

    CancelOrder unit test raw

---
+++ homeworks/homework-10/sample/TradingApp/TradingApp.Tests/TradingEngineCancelOrderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TradingApp.Tests
+{
+	[TestClass]
+	public class TradingEngineCancelOrderTests
+	{
+		[TestMethod]
+        public void CancelOrderByIdSouldCancelProcessingOrder()
+		{
+			var tradingEngine = new TradingEngine();
+			var orderId = tradingEngine.CreateOrder(TradingOrder.CreateOrderWithStatus(OrderStatus.Processing).OrderInfo);
+
+			tradingEngine.CancelOrder(orderId);
+
+			
+		}
+    }
+}
+
