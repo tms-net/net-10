4176c3d55cc48402db55020f9ed75483864ced0f
Nikita <mr.nikitakachura@gmail.com>
2023-07-18 17:43:40 +0200

    UTest for CancelOrder

--- homeworks/homework-10/sample/TradingApp/TradingApp.Tests/TradingEngineCancelOrderTests.cs
+++ homeworks/homework-10/sample/TradingApp/TradingApp.Tests/TradingEngineCancelOrderTests.cs
@@ -10,10 +10,21 @@ namespace TradingApp.Tests
 			var tradingEngine = new TradingEngine();
 			var orderId = tradingEngine.CreateOrder(TradingOrder.CreateOrderWithStatus(OrderStatus.Processing).OrderInfo);
 
-			tradingEngine.CancelOrder(orderId);
+			var isSuccessful = tradingEngine.CancelOrder(orderId);
 
-			
+			Assert.AreEqual(true, isSuccessful);
 		}
+
+		[TestMethod]
+		public void CancelOrderByIdShouldReturnFalseForUnexistingId()
+		{
+            var tradingEngine = new TradingEngine();
+			var orderId = "test_id";
+
+            var isSuccessful = tradingEngine.CancelOrder(orderId);
+
+			Assert.AreEqual(false, isSuccessful);
+        }
     }
 }
 
