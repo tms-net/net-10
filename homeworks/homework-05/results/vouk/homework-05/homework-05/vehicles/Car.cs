d533c9205fb994be9dcf20854e10fb830d60df92
Уразов Андрей <anshda6@gmail.com>
2023-07-24 19:13:19 +0300

    Test

--- homeworks/homework-05/results/vouk/homework-05/homework-05/vehicles/Car.cs
+++ homeworks/homework-05/results/vouk/homework-05/homework-05/vehicles/Car.cs
@@ -1,10 +1,11 @@
 ﻿using homework_05.interfaces;
 
-namespace homework_05.vehicles
+namespace homework_05.vehicles 
 {
     class Car : VehicleWithEngineBase
     {
         public int SittingPlacesCount { get; }
+        public int Test { get; }
 
         public Car(string mark, string model, string year, int horsepower, int maxSpeed, int sittingPlacesCount) : base(mark, model, year, horsepower, maxSpeed)
         {
