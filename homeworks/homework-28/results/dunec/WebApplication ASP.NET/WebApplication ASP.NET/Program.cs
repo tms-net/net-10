61d52c85b960d9f36ed40f3d89b864e0cc6b6ba8
PavelNTD <dunecpasha@yandex.ru>
2023-08-24 12:33:39 +0300

    first step

--- homeworks/homework-28/results/dunec/WebApplication ASP.NET/WebApplication ASP.NET/Program.cs
+++ homeworks/homework-28/results/dunec/WebApplication ASP.NET/WebApplication ASP.NET/Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+        app.MapGet("/", () => "Hello World!");
 
-app.Run();
+        app.MapGet("/request", (HttpContext context) => 
+        {
+            context.Response.StatusCode = 201;
+            context.Response.WriteAsync("ABOBA");
+        });
+
+        app.MapGet("/request/{filePath}", (string filePath) =>
+        {
+            File.ReadAllText(Path.Combine("resources", filePath));
+        });
+
+        app.Run();
+    }
+}
