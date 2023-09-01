22fd1913870c13e8d59aae32768a68121a5b62a7
Dmitry Andrienko <d.andrienko@live.com>
2023-09-01 17:25:35 +0300

    Add homework 33

---
+++ homeworks/homework-33/mvc/Lesson-33-Commerce/Lesson-33-Commerce/Models/ShopViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson_33_Commerce.Models
+{
+    public class ShopViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<ProductInfo> Products { get; set; }
+    }
+
+    public class ProductInfo
+    {
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal? OldPrice { get; set; }
+
+
+        // public string ProductUrl { get; set; }
+
+        public string Id { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
