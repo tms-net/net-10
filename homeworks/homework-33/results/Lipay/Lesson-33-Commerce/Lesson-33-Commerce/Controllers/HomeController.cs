00628f6081eff5933878381e211d23b8d0decaa2
Evgeny Lipay <lipay51131@mail.ru>
2023-09-02 12:56:27 +0300

    init commit

---
+++ homeworks/homework-33/results/Lipay/Lesson-33-Commerce/Lesson-33-Commerce/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Lesson_33_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson_33_Commerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult Product(string id)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
